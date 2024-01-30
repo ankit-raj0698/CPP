0281b32e1083a68eaaba2ca00a68ec6742af1d6e
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-01-30 23:33:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Maximize Toys/maximize-toys.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int toyCount(int N, int K, vector<int> arr)
+    {
+        // code here
+        sort(arr.begin(),arr.end());
+        int left=0;
+        int right=0;
+        int ans=0;
+        while(right < N && K > 0 ){
+            if(arr[right] <= K){
+                ans++;
+                K-=arr[right];
+            }
+            right++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, K;
+        cin>>N>>K;
+        vector<int> arr(N);
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        cout<<ob.toyCount(N, K, arr)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
