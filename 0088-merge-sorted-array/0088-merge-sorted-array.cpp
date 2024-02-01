9d12f0d23e5e8f0b13da9568b47e0f5310954528
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-02-01 15:36:38 +0530

    Time: 0 ms (100.00%), Space: 11 MB (6.95%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -17,7 +17,11 @@ public:
             }
             k--;
         }
-        
+        // Hmlog nums1 m hi change kr rhe (nums1 ke end se )
+        // max ele of nums1,nums2 end m hoga same process continue rakho
+        // to jb nums2 ke sare element traverse kr liye
+        // to ab bacha hua nums1 ka element to pehle se sorted hi h na
+        // because nums1 ke unn elements ko to kch kiye hi nhi
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
