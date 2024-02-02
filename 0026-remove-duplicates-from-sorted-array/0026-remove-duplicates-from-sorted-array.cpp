d3ce0a5b27a4caade223e93515157aec93d84732
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-02-02 17:21:28 +0530

    Time: 4 ms (88.37%), Space: 21.1 MB (14.97%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -10,7 +10,7 @@ public:
         // wahan pe khada hoke pucho ki usse 1 element pehle kaun h 
         // i.e.. "i-1" index pe kaun h 
         // Agar "i-1" index pe diff element h, current element i.e.."j" index se
-        // to mtlb "i" index pe insert kr skte h
+        // to mtlb "i" index pe current element i.e.. "j" index ko insert kr skte h
 
         for(int j=0;j<nums.size();j++){
             if(j == 0 || nums[i-1] != nums[j]){
