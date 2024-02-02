f349a51ae75cebba5f6a2ede1aa389d6128bc1d5
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-02-02 17:15:28 +0530

    Time: 3 ms (97.47%), Space: 21.1 MB (14.97%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+        int i=0;
+
+        // Acc to question : element can appear atmost once
+        // to i=0 ko "if" block m handle kro
+        // jis bhi index m new element insert hoga i.e "i" index
+        // wahan pe khada hoke pucho ki usse 1 element pehle kaun h 
+        // i.e.. "i-1" index pe kaun h 
+        // Agar "i-1" index pe diff element h, current element i.e.."j" index se
+        // to mtlb "i" index pe insert kr skte h
+
+        for(int j=0;j<nums.size();j++){
+            if(j == 0 || nums[i-1] != nums[j]){
+                nums[i++] = nums[j];
+            }
+        }
+       return i; 
+    }
+};
