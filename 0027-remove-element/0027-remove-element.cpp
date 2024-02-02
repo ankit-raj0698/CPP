05b817fe2b0a5275497fd0ebdb93b11aa2318cf1
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-02-02 13:57:17 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (17.56%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -4,6 +4,7 @@ public:
         
         int i=0,j=0;
         while(j<nums.size()){
+            //when ele is not equal to val swap with nums[i]
             if(nums[j]!=val){
                 swap(nums[i],nums[j]);
                 i++;
