c4ac09cc348c220c84e138fbd80377442f05a1ca
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-10 21:08:59 +0530

    Time: 78 ms (93.69%), Space: 73.2 MB (44.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         
-        int maxFreq=INT_MIN;
         unordered_map<int,int> count;
-        for(int i=0;i<nums.size();i++){
-            count[nums[i]]++;
-            maxFreq=max(maxFreq,count[nums[i]]);
-            if(maxFreq > 1){
+        for(int ele:nums){
+            count[ele]++;
+            if(count[ele] > 1)
                 return true;
-            }
         }
-      return false;  
+        return false;
     }
 };
