f4c8e5a01ec395388cdbfbb8dc67e59dbf7384be
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-10 22:38:20 +0530

    Time: 12 ms (61.04%), Space: 15.6 MB (7.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    /*
+    to store the answer create a vector , to store the elements create an unordered_map 
+    iterate through the input array , find the remainder i.e.. (target - sum)
+    if remainder is present in map so sum exists , update the answer vector with index of       remainder and index of the current element
+    else insert the current element in map(key -> element , value -> index)
+    */
     vector<int> twoSum(vector<int>& nums, int target) {
         
         vector<int> ans;
         unordered_map<int,int> mp;
+        
         for(int i=0;i<nums.size();i++){
+            
             int rem = target - nums[i];
+            
             if(mp.count(rem)){
                 ans.push_back(mp[rem]);
                 ans.push_back(i);
