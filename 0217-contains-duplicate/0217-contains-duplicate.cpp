751bfbffe284a743493da8bd6558e48f61672e27
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-10 22:23:19 +0530

    Time: 106 ms (29.00%), Space: 73.2 MB (25.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+    /*
+    create a map , iterate through the input array
+    increment the count of element of array in map
+    if count of any element is greater than 1 return false    
+    */
     bool containsDuplicate(vector<int>& nums) {
-        
         unordered_map<int,int> count;
         for(int ele:nums){
             count[ele]++;
