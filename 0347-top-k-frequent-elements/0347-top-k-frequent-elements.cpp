22a47327fddb6d81cf74c518b02eb0795bc511c9
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-11 10:02:05 +0530

    Time: 15 ms (32.61%), Space: 18.8 MB (11.80%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -37,4 +37,47 @@ public:
         }
         return ans;
     }
+    
+    /*
+    
+    // create an unordered map to count freq of each element in input array
+    // create a min heap to store {freq,element} of k elements
+    // if size of min heap exceeds k, pop from heap
+    // iterate through the min heap of k elements and store the elements in answer 
+    
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        typedef pair<int,int> P;
+        //create a map and store freq of each element
+        unordered_map<int,int> count;
+        for(auto ele:nums){
+            count[ele]++;
+        }
+        
+        //create a min heap to store freq and element
+        //But we want to store freq as well as element so use pair
+        priority_queue<P,vector<P>,greater<P>> pq;
+        for(auto ele:count){
+            int value = ele.first;
+            int freq = ele.second;
+            //in the min heap push {freq,value} first so that min heap is created on freq
+            pq.push({freq,value});
+            //when size of min heap exceeds k, pop 
+            if(pq.size()>k){
+                pq.pop();
+            }
+        }
+        
+       //store answer
+       vector<int> res;
+       while(!pq.empty()){
+           //element is present in second of pair
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+       return res;
+    }
+    
+    */
+    
 };
