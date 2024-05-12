e8097f05e6814a9f713300718843ae183a9b4b8e
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-12 18:57:43 +0530

    Time: 20 ms (37.91%), Space: 24.7 MB (26.55%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     
+    /*
+    check row wise if any number is repeated from 1-9 using set
+    check col wise if any number is repeated from 1-9 using set
+    check in a 3*3 box if any number is repeated from 1-9
+    for checking in a box create a method that will take rowStart, rowEnd, colStart, colEnd, board
+    and check if any number is repeated from 1-9 using set
+    rowStart and colStart value is incremented by 3 and rowEnd = rowStart +2, colEnd = colStart + 2
+    */
+    
+    //check if box(3*3) is valid sudoku
+    //Note: mistake done -> row <= rowEnd but i had done row < rowEnd
     bool traverseBox(int rowStart,int rowEnd,int colStart, int colEnd,vector<vector<char>>& board){
         
         unordered_set<char> st;
@@ -19,7 +30,7 @@ public:
     
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        //check row wise
+        //check row wise for any repeated no
         for(int row = 0; row < 9; row++){
             unordered_set<char> st;
             for(int col = 0; col < 9; col++){
@@ -32,7 +43,7 @@ public:
             }
         }
         
-        //check column wise
+        //check column wise  for any repeated no
         for(int col = 0; col < 9; col++){
             unordered_set<char> st;
             for(int row = 0; row < 9; row++){
@@ -45,7 +56,7 @@ public:
             }
         }
         
-        //check box wise
+        //check box wise  for any repeated no
         for(int row = 0; row < 9; row+=3){
             for(int col = 0; col < 9; col+=3){
                 int rowStart = row;
@@ -53,6 +64,7 @@ public:
                 int colStart = col;
                 int colEnd = col + 2;
                 
+                //check if box is valid sudoku
                 if(!traverseBox(rowStart,rowEnd,colStart,colEnd,board))
                     return false;
                 
