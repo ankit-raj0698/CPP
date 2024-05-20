5a760fcf5854a6991f8c98758a127fe3d71eb8b1
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-20 10:33:27 +0530

    Time: 122 ms (61.62%), Space: 73.9 MB (67.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    
+    int longestConsecutive(vector<int>& nums) {
+        
+        if(nums.size() == 0)
+            return 0;
+        
+        unordered_set<int> s;
+        for(auto ele:nums){
+            s.insert(ele);
+        }
+        int maxLength = INT_MIN;
+        for(auto ele:s){
+            if(s.find(ele-1) == s.end()){
+                int length = 1;
+                while(s.find(ele+length) != s.end())
+                    length++;
+                maxLength = max(maxLength,length);
+            }              
+        }
+        return maxLength;
+    }
+    
+    /*
+    // take a lastSmaller = INT_MIN and initial length to be 1
+    // iterate through the array starting from "0" to "n-1"
+    // check if "arr[i] - 1" is less than lastSmaller then it is part of consecutive sequence
+    // so update the length that is length++ and lastSmaller to be current element i.e.. arr[i]
+    // if "arr[i] == lastSmaller" then no need to do anything simply skip
+    // if "arr[i] != lastSmaller" so it means that we are getting a new sequence
+    // so update the length = 1 and lastSmaller to be current element i.e.. arr[i]
     int longestConsecutive(vector<int>& nums) {
         
         if(nums.size() == 0)
@@ -25,4 +55,5 @@ public:
         }        
         return maxLength;               
     }
+    */
 };
