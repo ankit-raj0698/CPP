d95dbe872be6b4d63edadef6fac51aded62aeda8
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-20 10:55:01 +0530

    Time: 120 ms (64.06%), Space: 74 MB (48.87%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     
+   /*
+   // insert all the elements of input array in an unordered set
+   // iterate through all the elements of unordered set
+   // if current element is nums[i] and nums[i] - 1 is present in the set Then
+   // it means that this is not the starting point of the sequence so skip it
+   // if current element is nums[i] and "nums[i] - 1 is not present" in the set then
+   // assign length = 1 and use a while loop
+   // iterate in the loop until nums[i] + length does not exist in the set
+   // in the while loop increment the length by 1
+   // check the maxLength and update accordingly
+   
+   */
     int longestConsecutive(vector<int>& nums) {
         
         if(nums.size() == 0)
@@ -10,12 +22,20 @@ public:
         for(auto ele:nums){
             s.insert(ele);
         }
-        int maxLength = INT_MIN;
+        
+        int maxLength = 1;
+        
         for(auto ele:s){
-            if(s.find(ele-1) == s.end()){
+            
+            if(s.find(ele-1) == s.end()){   
                 int length = 1;
-                while(s.find(ele+length) != s.end())
+                int n = ele;
+                
+                while(s.find(ele + 1) != s.end()){
                     length++;
+                    ele = ele + 1;
+                }  
+                
                 maxLength = max(maxLength,length);
             }              
         }
@@ -36,7 +56,7 @@ public:
             return 0;
         
         int n = nums.size();
-        int maxLength = INT_MIN;
+        int maxLength = 1;
         int lastSmaller = INT_MIN;
         int length = 1;
         sort(nums.begin(),nums.end());
