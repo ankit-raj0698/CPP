a01c8539b0258856ca08f502ffd9213c106328e4
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-23 23:52:32 +0530

    Time: 24 ms (27.62%), Space: 13.9 MB (22.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int left=0,right=0;
-        unordered_map<char,int> mp;
-        int ans=0;
-
-        while(right<s.length()){
-            
-            //update frequency of character 
-             mp[s[right]]++;
-
-            // when map size < window size so it means repeating character 
-            //so start removing character from window left 
-            //i.e.. update count of character at left
-            //If count of character at left is 0 so delete key from map 
-            //and increment left pointer
-            
-            while(mp.size() < (right-left+1) ){
-                mp[s[left]]--;
-                if(mp[s[left]] == 0){
-                    mp.erase(s[left]);
-                }
+        unordered_set<char> charSet;
+        int left = 0;
+        int length = 0;
+        
+        for(int right =0; right<s.size();right++){
+            //char "s[right]" is already in the set so shrink the window size from left
+            while(charSet.find(s[right]) != charSet.end()){
+                charSet.erase(s[left]);
                 left++;
             }
-            //If window size  == map size so all unique character so update ans
-            if(mp.size() == right-left+1){
-                ans=max(ans,right-left+1);
-            }
-            right++;
+            // when char is not in set so insert in the set
+            charSet.insert(s[right]);
+            length = max(length,right - left + 1);
         }
-        return ans;       
+        return length;
     }
 };
