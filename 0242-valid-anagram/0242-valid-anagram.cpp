e267e2b52e5e74a4b134e988a098886a8b7d36fc
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-23 10:25:19 +0530

    Time: 7 ms (76.63%), Space: 9.7 MB (19.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        
+    /*
+    check the size of s and t, if size is not same return false
+    create an unordered map and find the frequency of every element of s 
+    update frequency map accordingly
+    for each element of t, decrement the frequency of every element of map 
+    in the end, check if there exists any value(map.second) in map which is not 0
+    if yes then return false
+    */
+    
+     bool isAnagram(string s, string t) {       
         if(s.size()!=t.size()){
             return false;
         }
         
-        unordered_map<int,int> mp;
-        
+        unordered_map<char,int> mp;    
+         
         for(int i=0;i<s.size();i++){
             mp[s[i]]++;
-        }
-        
+        } 
         for(int i=0;i<t.size();i++){
             mp[t[i]]--;
         }
