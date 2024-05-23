96f60f82b5c8bb3a7f70136f1add29555068d254
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-23 12:11:00 +0530

    Time: 91 ms (35.80%), Space: 95.9 MB (31.72%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    /*
+    use sliding window left =0 and right = 1
+    iterate the loop until right pointer exceeds size of input array
+    if it is a profitable transaction then calculate profit and update maxprofit
+    otherwise move the left pointer to right as we have got a day where left is smaller than prev value
+    increment right everytime
+    */
     int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int maxProfit = 0;
         
-        int min=INT_MAX;
-        int maxProfit=0;
-        
-        for(auto p:prices){
-            
-            if(p < min){
-                min = p;
+        while(right < prices.size()){
+            if(prices[left] < prices[right]){
+                int profit = prices[right] - prices[left];
+                maxProfit = max(maxProfit,profit);              
             }
-            
-            int profit = p-min ;
-            if(profit > maxProfit){
-                maxProfit = profit;
+            else{
+                left = right;
             }
-            
+            right++;            
         }
-        return maxProfit;
+       return maxProfit; 
     }
 };
