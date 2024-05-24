e23fe8c8a5ff9ed0e052e73114d17e33d7b7817f
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-24 16:55:23 +0530

    Time: 3 ms (96.57%), Space: 8.1 MB (95.32%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> count(26,0);
+        int maxFreqCount = 0;
+        int length = 0;
+        
+        int left = 0, right =0;
+        
+        while(right < s.size()){
+            count[s[right] - 'A']++;
+            maxFreqCount = max(maxFreqCount,count[s[right] - 'A']);
+            
+            //LOGIC : when windowsize - maxFreqCount <= k then calculate result
+            // otherwise update the left pointer to shrink window
+            if( (right - left + 1) - maxFreqCount > k){
+                count[s[left] - 'A']--;
+                left++;
+            }
+            length = max(length, (right - left + 1));
+            right++;
+        }
+        return length;
+    }
+};
