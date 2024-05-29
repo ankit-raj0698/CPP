9e4c598df8de5cdd6e257e093f689c2bbfcc6d84
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-29 09:48:44 +0530

    Time: 19 ms (40.57%), Space: 11.8 MB (40.30%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    
+    int lengthOfLongestSubstring(string s) {
+        int left = 0, right = 0, maxLen = 0;
+        unordered_map<char,int> mp;
+        while(right < s.size()){
+            mp[s[right]]++;
+            
+            //all unique char in the current window
+            if(mp.size() == right - left + 1){
+                maxLen = max(maxLen, right - left +1);
+            }
+            // when there are duplicate char in current window so slide the window
+            else if(mp.size() < right - left + 1){
+                while(left <= right && mp.size() < right - left + 1){
+                    mp[s[left]]--;
+                    if(mp[s[left]] == 0){
+                        mp.erase(s[left]);
+                    }
+                    left++;
+                }
+            }
+            right++;
+        }
+        return maxLen;
+    }
+    
+    
+    
+    
+    
     /*
     sliding window left and right = 0
     use an unordered set to track unique elements seen till now
@@ -9,22 +39,22 @@ public:
     otherwise insert the current char s[right] in the set
     increment the right pointer everytime   
     */
-    int lengthOfLongestSubstring(string s) {
-        unordered_set<char> charSet;
-        int left = 0,right = 0;
-        int maxLength = 0;
+//     int lengthOfLongestSubstring(string s) {
+//         unordered_set<char> charSet;
+//         int left = 0,right = 0;
+//         int maxLength = 0;
         
-        while(right<s.size()){
-            //char "s[right]" is already in the set so shrink the window size from left
-            while(charSet.find(s[right]) != charSet.end()){
-                charSet.erase(s[left]);
-                left++;
-            }
-            // when char is not in set so insert in the set
-            charSet.insert(s[right]);
-            maxLength = max(maxLength,right - left + 1);
-            right++;
-        }
-        return maxLength;
-    }
+//         while(right<s.size()){
+//             //char "s[right]" is already in the set so shrink the window size from left
+//             while(charSet.find(s[right]) != charSet.end()){
+//                 charSet.erase(s[left]);
+//                 left++;
+//             }
+//             // when char is not in set so insert in the set
+//             charSet.insert(s[right]);
+//             maxLength = max(maxLength,right - left + 1);
+//             right++;
+//         }
+//         return maxLength;
+//     }
 };
