e94ee83701f2229b1744ac7268cb02eba04784e0
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-05-29 12:21:35 +0530

    Time: 19 ms (57.75%), Space: 10.4 MB (61.14%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,75 +2,49 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         
-        vector<int> need(256,0);
-        vector<int> have(256,0);
+        int left = 0, right = 0, minLen = INT_MAX;
+        int startIndex = -1;
+        unordered_map<char,int> mp;
         
-        int n=t.length();
-        
-        //store frequency of char of t
-        for(auto ch:t){
-            need[ch]++;
+        if(t.size() > s.size())
+            return "";
+        //count the freq of char in t
+        for(char c:t){
+            mp[c]++;
         }
         
-        int left=0;
-        int right=0;
-        int len=INT_MAX;
-        int start=-1;
+        int count = mp.size();
         
-        int count=0;
-        
-        while(right<s.length()){
-            
-            //store frequency of char of s in "have" vector
-            have[s[right]]++;
-            
-            
-            // jitni quantity kisi char ki jarurat hai agar utna hai ya usse kam h 
-            // to count badha do
-            
-            // why less than equal to?
-            // s: "AA"  t: "AA"
-            // at right=0 , s : A->1 , t : A->2 but count has to be increased
-            if(have[s[right]]<=need[s[right]]){
-                count++;
-            }          
-            
-            //Jb count t ke length ke barabar ho jaye to possible answer mil skta hai
-            if(count==n){
-                
-                // need[s[left]]==0 mtlb ye "have" vector m h
-                // pr iski jarurat nhi kyo ki ye char "need" m nhi h
-                
-                // have[s[left]]>need[s[left]] mtlb "have" m ye char jyada times h need se
-                // and hmlog ki min nikalna h to ignore kro mtlb left ko aage badhao
-                // and uska count ghatao vector me
-                while(need[s[left]]==0 || have[s[left]]>need[s[left]]){
-                    
-                    if(have[s[left]]>need[s[left]]){
-                        have[s[left]]--;
-                    }
-                    left++;
-                }
-                
-                // ek bar bekar ke char ko hta diye to ab left sahi jagah pr h
-                // len compare kr lo
-                if(right-left+1 < len){
-                    len=right-left+1;
-                    start=left;
-                }                                    
+        while(right < s.size()){
+            //check if char is present in map, if yes then decrement the freq 
+            if(mp.find(s[right]) != mp.end()){
+                mp[s[right]]--;
+                // when freq of the char is 0, decrement the count
+                if(mp[s[right]] == 0)
+                    count--;
             }
             
+                while(count == 0){           
+                    if(right - left + 1 < minLen){
+                        startIndex = left;
+                        minLen = right - left + 1;
+                    }
+                    
+                    // if s[left] is in map, then increment the freq of char in map
+                    if(mp.find(s[left]) != mp.end()){
+                        mp[s[left]]++;
+                        if(mp[s[left]] > 0)
+                            count++;
+                    }
+                    left++; // slide the window 
+                } 
             right++;
-        }
+            }
         
-        //Agar empty string hai ya "t" ka size "s" se bada hai to empty string return kro
-        if(start==-1){
+        if(startIndex == -1)
             return "";
-        }
         else{
-            //substring return kr do
-            return s.substr(start,len);
-        }
-               
+            return s.substr(startIndex,minLen);
+        }   
     }
 };
