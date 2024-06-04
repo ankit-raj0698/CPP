5b8c24ddff7735ddca33036bf84fecf5696685fb
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-04 14:37:56 +0530

    Time: 90 ms (65.27%), Space: 73 MB (71.95%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    /*
-    create an unordered map , iterate through the input array
-    find the frequency of every element of array using map
-    if count of any element is greater than 1 return false    
-    */
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int,int> count;
         for(int ele:nums){
