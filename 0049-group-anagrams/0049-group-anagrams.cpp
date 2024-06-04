4c00a6c5b76b0c64bb297d818b572340b6ee8664
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-04 14:48:29 +0530

    Time: 29 ms (50.30%), Space: 24.9 MB (49.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    /*
-    create a vector<vector<string>> to store answer
-    create a map (key -> string, value -> vector<string>)
-    iterate through each element of input string
-    store the sorted version of current element as key and push_back unsorted version of         current element as value in map
-    iterate through the map to store the values of map(vector<string>) in answer
-    return ans  
-    */
+    
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
         vector<vector<string>> ans;
