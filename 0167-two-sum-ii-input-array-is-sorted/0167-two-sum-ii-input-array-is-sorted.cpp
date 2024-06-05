16771b640abd5f0a5583a6b43395a7e9ab26af00
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-05 14:28:45 +0530

    Time: 15 ms (6.20%), Space: 18 MB (19.56%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    
-    /*
-    use two pointer approach, i= leftmost index and j= rightmost index of input array
-    if sum == target return index i,j (in question vector is 1 based indexing so do i+1,j+1)
-    if sum < target so we have to increase sum to go towards target so move right that is i++
-    if sum > target so we have to decrease sum to go towards target so move left that is j--
-    */
+   
     vector<int> twoSum(vector<int>& numbers, int target) {
         
         int i = 0;
