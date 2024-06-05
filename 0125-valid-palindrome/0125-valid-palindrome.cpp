5c02ef652419b65a26570cf53abbb593ab73f0c0
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-05 14:26:09 +0530

    Time: 8 ms (34.54%), Space: 8.7 MB (61.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    /*
-    use two pointer approach to solve this problem i=leftmost index and j=rightmost index
-    use a while loop till "i<j"
-    use another while loop to increment "i" till you don't encounter an alphanumeric character
-    use another while loop to decrement "j" till you don't encounter an alphanumeric character
-    our i and j are now pointing to alphanumeric character so compare them by converting them to lower
-    case first
-    if there is mismatch return false;
-    */
+    
     bool isPalindrome(string s) {
         
         int i = 0;
         int j = s.size()-1;
         
-        while(i < j){
-            while(!isalnum(s[i]) && i < j){
+        while( i < j ){
+            while( i < j && !isalnum(s[i]) ){
                 i++;
             }
-            while(!isalnum(s[j]) && i < j){
+            while( i < j && !isalnum(s[j]) ){
                 j--;
             }
-            if(tolower(s[i]) != tolower(s[j]))
+            if( tolower(s[i]) != tolower(s[j]) )
                 return false;
             
             i++;
