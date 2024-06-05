2b50b1150d9da5a652f20dc2e6ffb71d8f5acea6
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-05 12:11:26 +0530

    Time: 18 ms (43.83%), Space: 13.9 MB (23.39%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,45 +1,7 @@
 class Solution {
 public:
+ 
     
-    /*
-    int lengthOfLongestSubstring(string s) {
-        int left = 0, right = 0, maxLen = 0;
-        unordered_map<char,int> mp;
-        while(right < s.size()){
-            mp[s[right]]++;
-            
-            //all unique char in the current window
-            if(mp.size() == right - left + 1){
-                maxLen = max(maxLen, right - left +1);
-            }
-            // when there are duplicate char in current window so slide the window
-            else if(mp.size() < right - left + 1){
-                while(left <= right && mp.size() < right - left + 1){
-                    mp[s[left]]--;
-                    if(mp[s[left]] == 0){
-                        mp.erase(s[left]);
-                    }
-                    left++;
-                }
-            }
-            right++;
-        }
-        return maxLen;
-    }
-    */
-    
-    
-    
-    
-    /*
-    sliding window left and right = 0
-    use an unordered set to track unique elements seen till now
-    iterate till right reaches the last index of input string
-    if char "s[right]" is already in the set so shrink the window size from left
-    so delete the char at s[left] from set and increment left pointer
-    otherwise insert the current char s[right] in the set
-    increment the right pointer everytime   
-    */
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> charSet;
         int left = 0,right = 0;
