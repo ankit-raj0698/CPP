58d8c64eec3f5d7c2a09fea695af0a084137906c
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-06-05 12:41:26 +0530

    Time: 22 ms (50.20%), Space: 10.7 MB (39.29%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,50 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        
-        int left = 0, right = 0, minLen = INT_MAX;
-        int startIndex = -1;
+        string ans  = "";
         unordered_map<char,int> mp;
         
         if(t.size() > s.size())
-            return "";
-        //count the freq of char in t
-        for(char c:t){
-            mp[c]++;
+            return ans;
+        
+        for(auto ele:t){
+            mp[ele]++;
         }
         
         int count = mp.size();
+        int left = 0, right = 0;
+        int minlen = INT_MAX,  leftindex = -1;
         
         while(right < s.size()){
-            //check if char is present in map, if yes then decrement the freq 
             if(mp.find(s[right]) != mp.end()){
                 mp[s[right]]--;
-                // when freq of the char is 0, decrement the count
                 if(mp[s[right]] == 0)
                     count--;
             }
             
-                while(count == 0){           
-                    if(right - left + 1 < minLen){
-                        startIndex = left;
-                        minLen = right - left + 1;
-                    }
-                    
-                    // if s[left] is in map, then increment the freq of char in map
-                    if(mp.find(s[left]) != mp.end()){
-                        mp[s[left]]++;
-                        if(mp[s[left]] > 0)
-                            count++;
-                    }
-                    left++; // slide the window 
+            
+            while(count == 0){
+                if(right - left + 1 < minlen){
+                    leftindex = left;
+                    minlen = right - left + 1;
+                }
+                
+                if(mp.find(s[left]) != mp.end()){
+                    mp[s[left]]++;
+                    if(mp[s[left]] == 1)
+                        count++;
                 } 
-            right++;
+                left++;
             }
-        
-        if(startIndex == -1)
+          right++;  
+        }
+        if(leftindex == -1)
             return "";
         else{
-            return s.substr(startIndex,minLen);
-        }   
+            return s.substr(leftindex,minlen);
+        } 
     }
 };
