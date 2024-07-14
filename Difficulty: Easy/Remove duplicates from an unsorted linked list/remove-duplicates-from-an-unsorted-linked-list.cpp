082f5b206d1ca2c756d5a3f4aa169ac5cffb428f
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-07-14 17:43:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) 
+    {
+     // your code goes here
+     if(head == NULL || head->next == NULL)
+        return head;
+     unordered_set<int> s;
+     Node* curr = head;
+     Node* prev = nullptr;
+     
+     while(curr){
+         // check if data is already in set
+         if(s.find(curr->data) != s.end()){
+             Node* front = curr->next;
+             prev->next = curr->next;
+             curr = front;
+         }
+         else{
+             // data is not in set so do following
+             // insert in set
+             // update the prev pointer
+             // increment the curr pointer
+             s.insert(curr->data);
+             prev = curr;
+             curr = curr->next;
+         }
+     }
+     return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
