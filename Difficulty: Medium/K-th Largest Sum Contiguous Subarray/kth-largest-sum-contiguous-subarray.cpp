8141328555222aac8a3ffebddc2a41c56801fd9a
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-08-18 00:52:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
+++ Difficulty: Medium/K-th Largest Sum Contiguous Subarray/kth-largest-sum-contiguous-subarray.cpp
@@ -1,50 +1,60 @@
 //{ Driver Code Starts
-//Initial Template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-//User function Template for C++
 
-class Solution{
-public:
-    int kthLargest(vector<int> &Arr,int N,int K){
-        priority_queue<int,vector<int>,greater<int>> pq;
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
         
-        for(int i = 0; i<N; i++){
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int n = arr.size();
+    
+        // Generate all subarrays
+        for (int i = 0; i < n; i++) {
             int sum = 0;
-            for(int j = i; j < N; j++){
-                sum += Arr[j];
-                pq.push(sum);
-                
-                if(pq.size() > K)
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if(pq.size() < k)
+                    pq.push(sum);
+                else if(pq.top() < sum){
                     pq.pop();
+                    pq.push(sum);
+                }
             }
         }
+    
         return pq.top();
-        
     }
 };
 
 //{ Driver Code Starts.
 
-int main(){
-    
-    int T;
-    cin>>T;
-    while(T--){
-        int N,K;
-        cin>>N;
-        cin>>K;
-        vector<int> Arr(N);
-        for(int i=0;i<N;i++){
-            cin>>Arr[i];
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
         }
-        Solution ob;
-        int ans=ob.kthLargest(Arr,N,K);
-        cout<<ans<<endl;
+        string ks;
+        getline(cin, ks);
+        int k = stoi(ks);
+        Solution obj;
+        int res = obj.kthLargest(arr, k);
+        cout << res << endl;
+        // cout << "~" << endl;
     }
     return 0;
 }
+
 // } Driver Code Ends
