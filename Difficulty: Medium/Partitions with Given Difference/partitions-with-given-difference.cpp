162bfbd6dbc4626543fb52d1fca313f8c99fa85e
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-08-29 14:59:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -5,36 +5,39 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  
-    const int mod = 1e9 + 7;
-    int solve(int ind, int target, vector<int>& arr,vector<vector<int>> &dp){
-        if(ind < 0)
-            return target == 0 ? 1 : 0;
-            
-        if(dp[ind][target] != -1)
-            return dp[ind][target];
+    int mod = 1e9 + 7;
+    int solve(vector<int>& arr, int n, int ind, int sum, vector<vector<int>>&dp){
+        if(ind == n){
+            if(sum == 0)
+                return 1;
+            return 0;
+        }
         
-        int nottaken = solve(ind - 1, target, arr,dp) % mod;
-        
-        int taken = 0;
-        if(arr[ind] <= target)
-            taken = solve(ind - 1, target - arr[ind], arr,dp) % mod;
-        
-        return dp[ind][target] = (nottaken + taken) % mod;
-    }
-    int countPartitions(int n, int d, vector<int>& arr) {
-        // Code here
-        
-        int sum = 0;
-        for(int i = 0; i < n; i++)
-            sum += arr[i];
-        
-        if ((sum - d) < 0 || (sum - d) % 2 != 0)
+        if(ind > n || sum < 0)
             return 0;
             
-        int target = (sum - d)/2;
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return solve(n-1, target, arr, dp );
+        if(dp[ind][sum] != -1)
+            return dp[ind][sum];
+        
+        int notTake = solve(arr, n, ind + 1, sum, dp) % mod;
+        int take =  solve(arr, n, ind + 1, sum - arr[ind], dp) % mod;
+        
+        return dp[ind][sum] = (take + notTake) % mod;
+    }
+  
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+        
+        if(sum - d < 0 || (sum - d) % 2 != 0)
+            return 0;
+        
+        int target = (sum - d) / 2;
+        
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        
+        return solve(arr, n, 0, target, dp);
+
     }
 };
 
