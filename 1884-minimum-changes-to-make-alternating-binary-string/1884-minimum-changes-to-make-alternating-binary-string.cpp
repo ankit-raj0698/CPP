4d29451fd2c14ca89cbac8366ed3eeab061bfde8
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-16 18:01:47 +0530

    Time: 6 ms (43.39%), Space: 8.4 MB (69.39%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,22 +3,18 @@ public:
     int minOperations(string s) {
 
         int startWithZero = 0; // 010101
-        int startWithOne = 0; //  101010
+        int startWithOne; //  101010
         for(int i = 0; i < s.length(); i++){
             if(i % 2 == 0){
-                if(s[i] == '0')
-                    startWithOne++;
-                else 
+                if(s[i] == '1')
                     startWithZero++;
             }
             else{
                 if(s[i] == '0')
                     startWithZero++;
-                else
-                    startWithOne++;
             }
         }
-
+        startWithOne = s.size() - startWithZero;
         return min(startWithOne, startWithZero);
         
     }
