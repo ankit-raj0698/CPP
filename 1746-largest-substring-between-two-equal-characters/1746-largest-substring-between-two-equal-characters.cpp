399821b49d8a0e9c3c3d3da922a5d2e50f754e23
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-16 18:35:49 +0530

    Time: 4 ms (30.58%), Space: 8.5 MB (31.14%) - LeetHub

---
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+
+        unordered_map<char, int> mp;
+        int maxLen = -1;
+        for(int i = 0; i < s.size(); i++){
+            if(mp.find(s[i]) != mp.end())
+                maxLen = max(maxLen, i - mp[s[i]] - 1);
+            else
+                mp[s[i]] = i;
+        }
+       return maxLen; 
+    }
+};
