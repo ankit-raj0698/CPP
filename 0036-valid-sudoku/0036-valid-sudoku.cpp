5c174031b5590359b67c86a302b4a98212b2823c
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-17 11:42:37 +0530

    Time: 11 ms (91.45%), Space: 25.1 MB (36.24%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,66 +1,62 @@
 class Solution {
 public:
-    
-    //check if box(3*3) is valid sudoku
-    //Note: mistake done -> row <= rowEnd but i had done row < rowEnd
-    bool traverseBox(int rowStart,int rowEnd,int colStart, int colEnd,vector<vector<char>>& board){
-        
-        unordered_set<char> st;
+
+    bool checkBox(int rowStart, int rowEnd, int colStart, int colEnd, vector<vector<char>>& board){
+        unordered_set<char> s;
         for(int row = rowStart; row <= rowEnd; row++){
             for(int col = colStart; col <= colEnd; col++){
                 if(board[row][col] == '.')
                     continue;
-                if(st.find(board[row][col]) != st.end()){
+                if(s.find(board[row][col]) != s.end())
                     return false;
-                }
-                st.insert(board[row][col]);
+                
+                s.insert(board[row][col]);
             }
         }
-        return true;      
+        return true;
     }
-    
+
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        //check row wise for any repeated no
+        // check row wise
         for(int row = 0; row < 9; row++){
-            unordered_set<char> st;
+            unordered_set<char> s;
             for(int col = 0; col < 9; col++){
                 if(board[row][col] == '.')
                     continue;
-                if(st.find(board[row][col]) != st.end()){
+                if(s.find(board[row][col]) != s.end())
                     return false;
-                }
-                st.insert(board[row][col]);
+                
+                s.insert(board[row][col]);
             }
         }
-        
-        //check column wise  for any repeated no
+
+        // check col wise
         for(int col = 0; col < 9; col++){
-            unordered_set<char> st;
+            unordered_set<char> s;
             for(int row = 0; row < 9; row++){
                 if(board[row][col] == '.')
                     continue;
-                if(st.find(board[row][col]) != st.end()){
+                if(s.find(board[row][col]) != s.end())
                     return false;
-                }
-                st.insert(board[row][col]);
+                
+                s.insert(board[row][col]);
             }
         }
-        
-        //check box wise  for any repeated no
+
+        // check all the 9 boxes
         for(int row = 0; row < 9; row+=3){
             for(int col = 0; col < 9; col+=3){
                 int rowStart = row;
                 int rowEnd = row + 2;
                 int colStart = col;
                 int colEnd = col + 2;
-                
-                //check if box is valid sudoku
-                if(!traverseBox(rowStart,rowEnd,colStart,colEnd,board))
+
+                if(!checkBox(rowStart, rowEnd, colStart, colEnd, board))
                     return false;
-                
             }
         }
+
         return true;
     }
 };
