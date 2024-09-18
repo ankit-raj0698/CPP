09995f1d2e41231d842f8dd7d4935e5da8731a0b
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-18 18:41:32 +0530

    Time: 106 ms (86.24%), Space: 65.7 MB (72.78%) - LeetHub

---
+++ 2145-grid-game/2145-grid-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+
+        long long top = 0, bottom = 0, ans = LONG_MAX;
+
+        // calculate prefix sum of first row
+        for(int j = 0; j < grid[0].size(); j++)
+            top += grid[0][j];
+
+        // first robot can change its direction at each col
+        // so calculate how to minimise points to be collected by second robot
+        for(int j = 0; j < grid[0].size(); j++){
+            top -= grid[0][j];
+            ans = min(ans, max(top, bottom));
+            bottom +=  grid[1][j];
+        }
+        return ans;
+    }
+};
