5b857494c6c84edca1dd2957415bb120a733ff72
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-19 15:52:46 +0530

    Time: 5 ms (28.34%), Space: 11.2 MB (69.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
 
-        while(i>=0 && j>=0){
-            if(nums2[j]>=nums1[i]){
-                nums1[k]=nums2[j];
-                j--;
-            }
-            else{
-                nums1[k]=nums1[i];
-                i--;
-            }
-            k--;
+    // solved without using extra space
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int k = m + n - 1;
+        int i = m-1, j = n-1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j])
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
         }
-        // Hmlog nums1 m hi change kr rhe (nums1 ke end se )
-        // max ele of nums1,nums2 end m hoga same process continue rakho
-        // to jb nums2 ke sare element traverse kr liye
-        // to ab bacha hua nums1 ka element to pehle se sorted hi h na
-        // because nums1 ke unn elements ko to kch kiye hi nhi
-        while(j>=0){
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
+
+        while(i >= 0)
+            nums1[k--] = nums1[i--];
+        
+        while(j >= 0)
+            nums1[k--] = nums2[j--];
+
     }
 };
