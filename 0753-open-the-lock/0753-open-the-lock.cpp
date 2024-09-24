536b375f763d5cf01d5ed421ec47b50ade6a1b25
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-24 15:03:22 +0530

    Time: 98 ms (83.97%), Space: 38.7 MB (59.7%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.cpp
+++ 0753-open-the-lock/0753-open-the-lock.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
+
+        // use set to store all the deadends
         unordered_set<string> st(deadends.begin(), deadends.end());
 
         string start = "0000";
@@ -16,34 +18,42 @@ public:
         while(!q.empty()){
             int n = q.size();
 
+            // perform level order traversal
             while(n--){
                 string curr = q.front();
                 q.pop();
 
+                // when curr string is target
                 if(curr == target)
                     return level;
 
+                // try all possiblities on each char of string
+                // increasing and decreasing the char for each index
                 for(int i = 0; i < 4; i++){
                     char ch = curr[i];
 
+                    // increase the char val
                     curr[i] = ch == '9'? '0' : ch + 1;
+                    // when increased char is not in deadend, push in queue and set
                     if(st.find(curr) == st.end()){
                         st.insert(curr);
                         q.push(curr);
                     }
-
+                    // revert to original string and the decrease the char val
                     curr[i] = ch;
                     curr[i] = ch == '0'? '9' : ch - 1;
+                    // when decreased char is not in deadend, push in queue and set
                     if(st.find(curr) == st.end()){
                         st.insert(curr);
                         q.push(curr);
                     }
-
+                    // in the end revert to original string
                     curr[i] = ch;
                 }
             }
             level++;
         }
+        // when target can't be reachd from start
         return -1;
     }
 };
