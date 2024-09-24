0f663fa0ffdb7e479e97131edb0096f81ac55d6e
ankit-raj0698 <157926345+ankit-raj0698@users.noreply.github.com>
2024-09-24 22:43:05 +0530

    Time: 28 ms (8.15%), Space: 19.6 MB (13.9%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,55 +1,43 @@
 class Solution {
 public:
-    
-    bool topoSort(unordered_map<int, vector<int>> &adj,int numCourses, vector<int> &indegree){
-        queue<int> q;
-        
-        int count = 0;
-        // when indegree of a node is 0, push it in queue and increment the count
-        for(int i = 0; i < numCourses;i++){
-            if(indegree[i] == 0){
-                q.push(i);
-                count++;
-            }
-        }
-        
-        while(!q.empty()){
-            // find the front node
-            int u = q.front();
-            q.pop();
-            
-            // visit the neighbour of front node and decrement the indegree of neighbours
-            for(int v : adj[u]){
-                indegree[v]--;
-                // when indegree of a node is 0, push it in queue and increment the count
-                if(indegree[v] == 0){
-                    count++;
-                    q.push(v);
-                }
-            }
-        }
-        // if cycle doesn't exist so courses can be done so return true
-        if(count == numCourses){
-            return true;
-        }
-       
-        return false;
-        
-    }
-    
+    // if topological sort is possible then return true otherwise false
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
         unordered_map<int, vector<int>> adj;
-        vector<int> indegree(numCourses,0);
-        
-        // create the adjacency list 
-        for(vector<int> vec: prerequisites){
+        vector<int> indegree(numCourses, 0);
+        vector<int> ans;
+        queue<int> q;
+
+        for(auto &vec: prerequisites){
             int u = vec[0];
             int v = vec[1];
-            // 1--->0
+            // [ai, bi] => bi -> ai
             adj[v].push_back(u);
-            indegree[u]++;         
+            indegree[u]++;
         }
-      return topoSort(adj, numCourses,indegree);
+
+        // push all the nodes in queue whose indegree is 0
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            ans.push_back(u);
+
+            // decrease the indegree of adj[u]
+            for(int v: adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+        // when topological sort is possible
+        if(ans.size() == numCourses)
+            return true;
+        
+        return false;
+        
     }
 };
