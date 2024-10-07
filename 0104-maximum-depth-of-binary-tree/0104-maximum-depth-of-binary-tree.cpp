33a1a8db1f0dbc913524b8f719b018c17a607383
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-10-07 18:46:19 +0530

    Time: 8 ms (36.5%), Space: 17.8 MB (7.91%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -12,20 +12,14 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
-        //Base case : smallest valid/invalid input
-        //For tree smallest valid input would be null
-        if(root==nullptr)
+
+        if(!root)
             return 0;
         
-        //Hypothesis : find ht of root->left and root->right
-        // for func(n) find func(n-1)      
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        
-        //Induction : Extra work to be done
-        //To calculate ht of tree find max ht of left,right subtree and add 1 for root node
-        return 1 + max(left,right);    
+        int leftSubtree = maxDepth(root->left);
+        int rightSubtree = maxDepth(root->right);
+
+        return 1 + max(leftSubtree, rightSubtree);
         
     }
 };
