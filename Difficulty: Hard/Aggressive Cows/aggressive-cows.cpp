890a6f1f334fa59822e1afe878253edff9eb9519
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-10-31 01:44:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
@@ -14,7 +14,7 @@ class Solution {
     bool isPossible(vector<int> &stalls, int k, int mid){
         int cowCount = 1;
         int lastPos = stalls[0];
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             if(stalls[i] - lastPos >= mid){
                 cowCount++;
                 if(cowCount == k)
