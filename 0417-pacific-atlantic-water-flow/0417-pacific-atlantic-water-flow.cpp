9172466b38218a7bd41c794f14c28d612c15925f
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-03 14:27:50 +0530

    Time: 28 ms (17.36%), Space: 22.4 MB (48.37%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,62 +1,56 @@
 class Solution {
 public:
-
     int n, m;
-
-    void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<int>>& visited, int prev){
+    vector<pair<int, int>> direction = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    void dfs(vector<vector<int>>& heights, int i, int j, vector<vector<int>> &visited, int prevHeight){
         // out of bound, visited, blocked
-        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || heights[i][j] < prev)
+        // path is blocked when current cell height is less than prev height
+        // as we are checking from one ocean to another and not from any cell
+        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || heights[i][j] < prevHeight)
             return;
-        // mark the current cell visited
+        
         visited[i][j] = 1;
 
-        // traverse all possible direction
-        dfs(heights, i + 1, j, visited, heights[i][j]);
-        dfs(heights, i - 1, j, visited, heights[i][j]);
-        dfs(heights, i, j + 1, visited, heights[i][j]);
-        dfs(heights, i, j - 1, visited, heights[i][j]);
+        for(auto dir: direction){
+            int row = i + dir.first;
+            int col = j + dir.second;
 
+            dfs(heights, row, col, visited, heights[i][j]);
+        }
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         n = heights.size();
         m = heights[0].size();
 
-        vector<vector<int>> ans;
-        if(n == 1 && m == 1){
-            ans.push_back({0,0});
-            return ans;
-        }
-            
-        // create two visited array to track pacific and atlantic ocean
         vector<vector<int>> pacific(n, vector<int>(m,0));
         vector<vector<int>> atlantic(n, vector<int>(m,0));
 
-        // do dfs for first row and first col
+        // check if you can reach from pacific to atlantic
+        // check for first row and col
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if((i == 0 || j == 0 ) && !pacific[i][j])
+                if((i == 0 || j == 0) && !pacific[i][j])
                     dfs(heights, i, j, pacific, -1);
             }
-                
         }
 
-        // do dfs for last row and last col
+        // check if you can reach from atlantic to pacific
+        // check for last row and col
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if((i == n - 1 || j == m - 1 ) && !atlantic[i][j])
+                if((i == n - 1 || j == m - 1) && !atlantic[i][j])
                     dfs(heights, i, j, atlantic, -1);
-            }              
-        }
-
-        // intesection of pacific and atlantic visited array
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(pacific[i][j] == 1 && atlantic[i][j] == 1){
-                    ans.push_back({i,j});
-                }
             }
         }
 
-        return ans;       
+        // find the intersection of both the visited array
+       vector<vector<int>> ans;
+       for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(atlantic[i][j] && pacific[i][j])
+                    ans.push_back({i,j});
+            }
+        } 
+        return ans;
     }
 };
