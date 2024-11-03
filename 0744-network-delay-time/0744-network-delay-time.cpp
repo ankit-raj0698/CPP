45e77fffa2593e0405012f725c538e4340ecced1
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-03 16:24:12 +0530

    Time: 86 ms (46.29%), Space: 47.6 MB (18.85%) - LeetHub

---
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        typedef pair<int,int> t;
+        unordered_map<int, vector<t>> adj;
+        for(auto vec: times){
+            int u = vec[0];
+            int v = vec[1];
+            int wt = vec[2];
+
+            adj[u].push_back({v,wt});
+        }
+
+        priority_queue<t, vector<t>, greater<t>> pq;
+        vector<int> dist(n+1, 1e9);
+        pq.push({0,k});
+        dist[k] = 0;
+        
+
+        while(!pq.empty()){
+            auto [du, u] = pq.top();
+            pq.pop();
+
+            for(auto [v,wt] : adj[u]){
+                if(du + wt < dist[v]){
+                    dist[v] = du + wt;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+        int ans = -1;
+        for(int i = 1; i <= n; i++){
+            ans = max(ans, dist[i]);
+        }
+        return ans == 1e9 ? -1 : ans;
+    }
+};
