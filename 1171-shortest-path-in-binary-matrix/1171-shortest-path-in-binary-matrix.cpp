dd36d5c13bbd8073d69d18492a72c4171933eefe
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-03 14:47:33 +0530

    Time: 26 ms (47.88%), Space: 24.5 MB (57.24%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,47 +2,39 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        // when source or dest is not vacant
+        vector<vector<int>> visited(n, vector<int>(n,0));
+        queue<pair<int,int>> q;
+        vector<pair<int,int>> direction = {{1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
         
-        vector<vector<int>> visited(n, vector<int>(n, 0));
-        vector<pair<int,int>> direction = { {1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {-1,1}, {-1,-1}, {1,-1}};
-        queue<pair<int, int>> q;
-
-        // push the source in queue & mark it visited
         q.push({0,0});
         visited[0][0] = 1;
 
-        // since we are counting the source cell as well in path length so start from 1
         int level = 1;
         while(!q.empty()){
             int size = q.size();
-
             while(size--){
-                // find the co-ordinates of current cell to be popped
                 auto [i,j] = q.front();
                 q.pop();
 
-                // if current cell is destination then return level
                 if(i == n-1 && j == n-1)
                     return level;
-
-                // explore all 8 directions
+                
                 for(auto dir: direction){
                     int row = i + dir.first;
                     int col = j + dir.second;
 
-                    // within grid, not visited and vacant then mark visited and push in queue
-                    if(row >= 0 && row < n && col >= 0 && col < n && !visited[row][col] && grid[i][j] == 0){
+                    if(row >= 0 && row < n && col >= 0 && col < n && !visited[row][col] && grid[row][col] == 0){
                         visited[row][col] = 1;
                         q.push({row, col});
                     }
                 }
+
             }
-            level++;         
+            level++;
         }
-        // if not possible to reach destination return -1
         return -1;
     }
 };
