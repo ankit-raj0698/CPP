1da8a9b6d7494bf627b2fdd9d41199b89fe0a170
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-07 14:33:12 +0530

    Time: 0 ms (100%), Space: 9.2 MB (77.22%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -4,6 +4,8 @@ public:
 
         vector<int> dp(n + 1, 0);
         for(int i = 1; i <= n; i++){
+            // find the last bit if it is 1 or 0
+            // reduce the curr no to half
             dp[i] = (i % 2) + dp[i / 2];
         }
         return dp;
