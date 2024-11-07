bd62039d14484dc4a4c3e0dd557ea787d2baea5b
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-07 10:16:25 +0530

    Time: 56 ms (6.74%), Space: 45.4 MB (7.54%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     
-    int solve(vector<int>& nums, int  ind, int sum, vector<vector<long>> &dp){
+    int solve(vector<int>& nums, int  ind, int rem, vector<vector<int>> &dp){
         if(ind < 0){
-            if(sum % 3 == 0)
+            if(rem % 3 == 0)
                 return 0;
             return -1e9;
         }
 
-        if(dp[ind][sum] != -1)
-            return dp[ind][sum];
+        if(dp[ind][rem] != -1)
+            return dp[ind][rem];
 
-        int not_take = solve(nums, ind - 1, sum, dp);
-        int take = nums[ind] + solve(nums, ind - 1, (sum + nums[ind])% 3, dp);
+        int not_take = solve(nums, ind - 1, rem, dp);
+        int take = nums[ind] + solve(nums, ind - 1, (rem + nums[ind])% 3, dp);
 
-        return dp[ind][sum] = max(not_take , take);
+        return dp[ind][rem] = max(not_take , take);
     }
     int maxSumDivThree(vector<int>& nums) {
         int n = nums.size();
-        //long long sum = accumulate(nums.begin(), nums.end(), 0);
-        vector<vector<long>> dp(n, vector<long>(3, -1));
+        vector<vector<int>> dp(n, vector<int>(3, -1));
         return  solve(nums, n-1, 0, dp);
     }
 };
