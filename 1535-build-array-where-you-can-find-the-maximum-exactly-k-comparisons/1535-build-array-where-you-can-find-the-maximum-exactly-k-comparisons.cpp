645acce382cfdf49f0747c245de7cbcc7912e10f
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-07 14:19:29 +0530

    Time: 135 ms (47.13%), Space: 8.6 MB (52.59%) - LeetHub

--- 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1535-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int mod = 1e9 + 7;
     int dp[51][101][52];
-    int solve(int n, int ind, int m, int lastMax, int k, int searchCost){
+    int solve(int n, int m, int k, int ind, int lastMax, int searchCost){
 
+        // invalid state, no need of further recursion
         if(searchCost > k)
             return 0;
-
+        
+        // we have filled all the elements of array with values 1 .... m
         if(ind == n){
+            // when searchcost = k, so we found a way
              if(searchCost == k)
                 return 1;
             return 0;
@@ -18,10 +21,10 @@ public:
 
         int ans = 0;
         for(int x = 1; x <= m; x++){
-            if(x > lastMax)
-                ans = (ans + solve(n, ind + 1, m, x, k, searchCost+1)) % mod;
-            else   
-                ans = (ans + solve(n, ind + 1, m, lastMax, k, searchCost))  % mod;
+            if(x > lastMax) //curr element greater than prev max, increase the search cost
+                ans = (ans + solve(n, m , k, ind + 1, x, searchCost+1)) % mod;
+            else // curr element is less than prev max element 
+                ans = (ans + solve(n, m, k, ind + 1, lastMax,searchCost))  % mod;
             
         }
         return dp[ind][lastMax][searchCost] = ans % mod;
@@ -29,6 +32,6 @@ public:
     int numOfArrays(int n, int m, int k) {
         
         memset(dp, -1, sizeof(dp));
-        return solve(n, 0, m, 0, k, 0);
+        return solve(n, m, k, 0, 0, 0);
     }
 };
