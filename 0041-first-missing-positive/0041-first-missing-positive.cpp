e81a7b57e7263d39406f4d8c5cb8e34b538c4f34
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-13 15:28:30 +0530

    Time: 8 ms (41.18%), Space: 53.9 MB (53.99%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-
         int n = nums.size();
-        // we are expecting nums will contain 1 ..... n
-        for(int i = 0; i < n; i++){
-            int ele = nums[i]; // map it with index ele - 1
 
-            // consider only no. between 1 to n and ignore rest
-            // when element is not at its correct index , move it to correct place
+        for(int i = 0; i < n; i++){
+            int ele = nums[i];
+
             if(nums[i] >= 1 && nums[i] <= n && nums[i] != nums[ele - 1]){
-                swap(nums[i], nums[ele - 1]);
-                i--; // so that we again check the index after swap
+                swap(nums[i], nums[ele-1]);
+                i--;
             }
         }
 
-        for(int i = 0; i < n; i++){
-            if(nums[i] != i + 1)
-                return i + 1;
+        for(int i = 0;  i < n; i++){
+            if(nums[i] != i+1)
+                return i+1;
         }
-
-        // when all no from 1 to n is present in that case return n + 1
         return n + 1;
-        
     }
 };
