bb82c00f394683116932ee8f2ec72bc74766dbbb
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-15 13:19:22 +0530

    Time: 119 ms (65.9%), Space: 10.1 MB (98.73%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -5,13 +5,20 @@ public:
         int n = nums.size();
         memset(dp, 0, sizeof(dp));
 
+        // l denotes length
         for(int l = 1; l <=n; l++){
+            // i goes from to 0 to last valid row
             for(int i = 0; i <= n-l; i++){
+                // l = j - i + 1
                 int j = i  + l - 1;
+                // k can be anything from i to j
                 for(int k = i; k <=j; k++){
+                    // when k == i then dp[i][i-1] which is invalid so handle it
                     int left = i == k ? 0 : dp[i][k-1];
+                    // when k == j then dp[j+1][j] which is invalid so handle it
                     int right = j == k ? 0 : dp[k+1][j];
 
+                    // handle out of bound case for i and j
                     int val = (i == 0 ? 1 :nums[i-1]) * nums[k] * (j == n-1 ? 1 : nums[j+1]);
 
                     dp[i][j] = max(dp[i][j], left + val + right);
@@ -19,6 +26,7 @@ public:
             }
         }
 
+// return first row last col val
         return dp[0][n-1];
         
     }
