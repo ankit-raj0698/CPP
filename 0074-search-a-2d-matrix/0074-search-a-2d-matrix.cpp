f3b25a97b0e712ed678283972ab6464d0cf3864a
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-24 22:06:00 +0530

    Time: 0 ms (100%), Space: 12.2 MB (39.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -20,9 +20,30 @@ public:
         n = matrix.size();
         m = matrix[0].size();
 
+        /*
+        // this approach will take O(n + log m)
         for(int i = 0; i < n; i++){
             if(target >= matrix[i][0] && target <= matrix[i][m-1])
                 return binarySearch(matrix[i], target);
+        }
+        */
+
+        int low = 0, high = n*m - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+
+            // convert the 1D coordinate to 2d coordinate
+            int row = mid / m;
+            int col = mid % m;
+
+            // matrix[row][col] is our actual mid value
+
+            if(matrix[row][col] == target)
+                return true;
+            else if(target < matrix[row][col])
+                high = mid - 1;
+            else 
+                low = mid + 1;
         }
 
         return false;
