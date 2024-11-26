8d805ef660b5093f5864402dce07ea5157300f66
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-27 02:17:25 +0530

    Time: 215 ms (52.72%), Space: 146.5 MB (35.73%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,52 +1,70 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int start, int mid, int end){
-        // a[start ... mid]
-        // b[mid + 1 ... end]
-        // size = upper - lower + 1
+    int partition(vector<int> &nums, int start, int end){
+        int pos = start;
 
-        // declare size of left and right array
-        int n = mid - start + 1;
-        // end - (mid + 1) - 1
-        int m = end - mid;
-
-        vector<int> left(n);
-        vector<int> right(m);
-
-        // copy elements of original array to left and right array
-        for(int i = 0; i < n; i++)
-            left[i] = nums[start + i];
+        // whatever no is less than equal to last element, swap it with pos
+        for(int i = start; i < end; i++){
+            if(nums[i] <= nums[end]){
+                swap(nums[i], nums[pos]);
+                pos++;
+            }
+        }
+        // put the pivot element at correct pos
+        swap(nums[pos], nums[end]);
+        return pos;
+    }
+    void quicksort(vector<int> &nums, int start, int end){
+        // base case : 0 or 1 element left
+        if(start >= end)
+            return;
         
-        for(int i = 0; i < m; i++)
-            right[i] = nums[mid + 1 + i];
+        int pivot = partition(nums, start, end); // find the pivot index
+        quicksort(nums, start, pivot-1); // left part
+        quicksort(nums, pivot+1, end); // right part
+    }
 
-        // merge left and right array into original array
-        int i = 0, j = 0, k = start;
-        while(i < n && j < m){
-            if(left[i] <= right[j]){
-                nums[k++] = left[i++];
+    void merge(vector<int> &nums, int start, int mid, int end){
+        vector<int> temp(end - start + 1);
+        int left = start, right = mid + 1;
+        int ind = 0;
+        while(left <= mid && right <= end){
+            if(nums[left] <= nums[right]){
+                temp[ind++] = nums[left++];
             }
             else
-                nums[k++] = right[j++];
+                temp[ind++] = nums[right++];
         }
 
-        while(i < n)
-            nums[k++] = left[i++];
-        while(j < m)
-            nums[k++] = right[j++];
+        while(left <= mid){
+            temp[ind++] = nums[left++];
+        }
 
-    }
+        while(right <= end)
+            temp[ind++] = nums[right++];
 
-    void mergeSort(vector<int>& nums, int start, int end){
-        if(start < end){
-            int mid = start + (end - start)/2;
-            mergeSort(nums, start, mid);
-            mergeSort(nums, mid + 1, end);
-            merge(nums, start, mid, end);
+        ind = 0;
+        while(start <= end){
+            nums[start++] = temp[ind++];
         }
     }
+
+
+    void mergesort(vector<int> &nums, int start, int end){
+        if(start == end)
+            return;
+        int mid = start + (end - start)/2;
+        mergesort(nums, start, mid);
+        mergesort(nums, mid + 1, end);
+        merge(nums, start, mid, end);
+    }
+
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size()-1);
+
+        int start = 0, end = nums.size()-1;
+        //quicksort(nums, start, end);
+        mergesort(nums, start, end);
         return nums;
+        
     }
 };
