5fcb9727a884b9d97d841727d36d5123a59feed2
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-27 16:37:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
@@ -1,60 +1,41 @@
 //{ Driver Code Starts
 #include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+   // Redirecting input from file
+  freopen("/Users/debojyoti.mandal/expt/input.txt", "r", stdin);
+
+  // Redirecting output to file
+  freopen("/Users/debojyoti.mandal/expt/output.txt", "w", stdout);
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
 };
 
-
-void print(Node *root)
-{
+void print(Node *root) {
     Node *temp = root;
-    while(temp!=NULL)
-    {
-    cout<<temp->data<<" ";
-    temp=temp->next;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
 }
-Node* removeDuplicates(Node *root);
-int main() {
-	// your code goes here
-	int T;
-	cin>>T;
 
-	while(T--)
-	{
-		int K;
-		cin>>K;
-		Node *head = NULL;
-        Node *temp = head;
 
-		for(int i=0;i<K;i++){
-		int data;
-		cin>>data;
-			if(head==NULL)
-			head=temp=new Node(data);
-			else
-			{
-				temp->next = new Node(data);
-				temp=temp->next;
-			}
-		}
-		
-		Node *result  = removeDuplicates(head);
-		print(result);
-		cout<<endl;
-	}
-	return 0;
-}
 // } Driver Code Ends
-
-
 /*
 struct Node {
   int data;
@@ -65,23 +46,67 @@ struct Node {
   }
 };*/
 
-//Function to remove duplicates from sorted linked list.
-Node *removeDuplicates(Node *head)
-{
- // your code goes here
- if(head == nullptr || head->next == nullptr)
+class Solution {
+  public:
+    // Function to remove duplicates from sorted linked list.
+    Node* removeDuplicates(Node* head) {
+        
+        if(!head || !head->next)
+            return head;
+
+        // code here
+        Node* curr = head;
+        while(curr->next){
+            Node* front = curr->next;
+            if(curr->data == front->data){
+                curr->next = front->next;
+                delete front;
+            }
+            else
+                curr = curr ->next;
+        }
     return head;
-    
- // we have min 2 nodes
- Node* curr = head;
- while(curr->next != nullptr){
-     Node* front = curr->next;
-     if(curr->data == front->data)
-        curr->next = front->next;
-     else 
-        curr = curr->next;
- }
- 
- return head;
- 
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    // your code goes here
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+        Solution obj;
+        int data = arr[0];
+        Node *head = new Node(data);
+        Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        Node *result = ob.removeDuplicates(head);
+        print(result);
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
 }
+// } Driver Code Ends
