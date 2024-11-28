1c86d71c38511e2d45545fc8280c0d49c36bda31
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-11-28 21:46:40 +0530

    Time: 0 ms (100%), Space: 15.3 MB (6.02%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,58 +1,27 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int mid){
-        if(nums[mid] < nums[0])
-            return true;
-        return false;
-    }
-
-    int getPivot(vector<int>& nums){
-        int low = 0, high = nums.size()-1;
-        int ans = 0;
-        while(low  <= high){
-            int mid = low + (high - low)/2;
-            if(check(nums, mid)){
-                ans = mid;
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
-       return ans; 
-    }
-
-    int binarySearch(vector<int> &nums, int target, int low , int high){
-
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
         while(low <= high){
             int mid = low + (high - low)/2;
             if(nums[mid] == target)
-                return mid;           
-            else if(target < nums[mid])
-                high = mid - 1;
-            else
-                low = mid + 1;
-        }
-        return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-       int pivotIndex =  getPivot(nums);
-
-
-        // handle case when array is not rotated
-        if(pivotIndex == 0)
-            return binarySearch(nums, target, 0, nums.size() - 1);
-
-        // if pivot element is the target
-       if(nums[pivotIndex] == target)
-            return pivotIndex;
-        
-        // if target < first then search in the second half
-        if(target < nums[0])
-            return binarySearch(nums, target, pivotIndex + 1, nums.size() - 1);
-        else 
-            return binarySearch(nums, target, 0, pivotIndex - 1);
+                return mid;
             
+            else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else 
+                    low = mid + 1;
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
 
+        return -1;
+        
     }
 };
