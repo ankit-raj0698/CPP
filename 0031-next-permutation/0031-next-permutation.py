fa15347b631222064daec6e2514cd487256942b5
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-12-17 09:03:51 +0530

    Time: 0 ms (100%), Space: 17.4 MB (9.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,25 +1,26 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        # find the index of element such that nums[i] < nums[i+1]
+        
+        #find the index of the element such that nums[i] < nums[i+1]
         i = len(nums) - 2
         index = -1
         while i >= 0:
-            if nums[i] < nums[i + 1]:
+            if nums[i] < nums[i+1]:
                 index = i
                 break
             i -= 1
         
-
-        if index != -1:
+        #find the index of the element which is just greater than nums[index]
+        #swap it with nums[index]
+        if i != -1:
             j = len(nums) - 1
-            # find the index of element which is greater than nums[index]
-            while nums[j] <= nums[index]:
+            while j >= 0:
+                if nums[j] > nums[index]:
+                    nums[index], nums[j] = nums[j], nums[index]
+                    break
                 j -= 1
-            # swap nums[j] and nums[index]
-            nums[j], nums[index] = nums[index], nums[j]
-            
-        # reverse the part after index + 1
+        
+        #reverse the list from index+1 to end
         nums[index+1:] = reversed(nums[index+1:])
+
+        
