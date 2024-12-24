9d9543c40cbe1b2fbd706901ca321d39afa784b5
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2024-12-24 18:19:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Prime Number/prime-number.cpp
+++ Difficulty: Basic/Prime Number/prime-number.cpp
@@ -8,22 +8,11 @@ class Solution {
   public:
     bool isPrime(int n) {
         // code here
-        int cnt = 0;
-        for(int i = 1; i * i <= n; i++){
-            
-            if(n % i == 0){
-                // first factor
-                cnt++;
-                // second factor
-                if((n / i) != i)
-                    cnt++;
-            }
+        for(int i = 2; i < n; i++){
+            if(n % i == 0)
+                return false;
         }
-        
-        if(cnt == 2)
-            return true;
-        
-        return false;
+        return true;
     }
 };
 
