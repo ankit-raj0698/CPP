e9c0108b11112bb23bfdb43cf831e1afe5e31312
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-01-10 11:12:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (41.4%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -4,14 +4,14 @@ public:
 
         if(n == 1)
             return true;
-
+        // odd no and 0 can't be power of 2
         if(n % 2 == 1 || n == 0)
             return false;
         
         return solve(n/2);
     }
     bool isPowerOfTwo(int n) {
-        
+        // 0 and -ve no can't be power of 2
         if(n <= 0)
             return false;
         
