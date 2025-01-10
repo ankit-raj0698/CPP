df6e062c1bf101dd0a6417e4f9c48d2516351804
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-01-10 11:06:17 +0530

    Time: 0 ms (100%), Space: 8 MB (15.57%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool solve(int n){
+
         if(n == 1)
             return true;
+
         if(n % 2 == 1 || n == 0)
             return false;
         
         return solve(n/2);
     }
     bool isPowerOfTwo(int n) {
+        
         if(n <= 0)
             return false;
         
