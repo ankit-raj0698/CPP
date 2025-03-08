7bda769705258930f5540c8cbf341463233e5269
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-08 10:17:33 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& nums, int target, int low, int high){
+    int binarySearchIterative(vector<int>& nums, int target, int low, int high){
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] == target)
+                return mid;
+            
+            // check if array from low to mid is sorted
+            if(nums[low] <= nums[mid]){
+                // check if target is in this half
+                if(nums[low] <= target && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // second half is sorted
+            else{
+                // check if target is in this half
+                if(nums[mid] <= target && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+    int binarySearchRecursive(vector<int>& nums, int target, int low, int high){
         if(low > high)
             return -1;
         
@@ -10,45 +38,23 @@ public:
         
         if(nums[low] <= nums[mid]){
             if(nums[low] <= target && target <= nums[mid])
-                return binarySearch(nums, target, low, mid - 1);
+                return binarySearchRecursive(nums, target, low, mid - 1);
             else
-                return binarySearch(nums, target, mid+1, high);
+                return binarySearchRecursive(nums, target, mid+1, high);
         }
         else{
             if(nums[mid] <= target && target <= nums[high])
-                return binarySearch(nums, target, mid + 1, high);
+                return binarySearchRecursive(nums, target, mid + 1, high);
             else
-                return binarySearch(nums, target, low, mid - 1);
+                return binarySearchRecursive(nums, target, low, mid - 1);
         }
     }
+
     int search(vector<int>& nums, int target) {
         
         int low = 0, high = nums.size() - 1;
-        return binarySearch(nums, target, low, high);
+        return binarySearchIterative(nums, target, low, high);
+        // return binarySearch(nums, target, low, high);
 
-        // while(low <= high){
-        //     int mid = low + (high - low)/2;
-
-        //     if(nums[mid] == target)
-        //         return mid;
-            
-        //     // check if array from low to mid is sorted
-        //     if(nums[low] <= nums[mid]){
-        //         // check if target is in this half
-        //         if(nums[low] <= target && target <= nums[mid])
-        //             high = mid - 1;
-        //         else
-        //             low = mid + 1;
-        //     }
-        //     // second half is sorted
-        //     else{
-        //         // check if target is in this half
-        //         if(nums[mid] <= target && target <= nums[high])
-        //             low = mid + 1;
-        //         else
-        //             high = mid - 1;
-        //     }
-        // }
-        // return -1;
     }
 };
