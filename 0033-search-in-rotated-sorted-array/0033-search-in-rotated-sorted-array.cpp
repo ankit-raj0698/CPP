ba9239d0fd5f9817fe27ef7c1b16a0ec2598834f
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-08 10:15:03 +0530

    Time: 0 ms (100%), Space: 15.2 MB (33.35%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& nums, int target, int low, int high){
+        if(low > high)
+            return -1;
+        
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target)
+            return mid;
+        
+        if(nums[low] <= nums[mid]){
+            if(nums[low] <= target && target <= nums[mid])
+                return binarySearch(nums, target, low, mid - 1);
+            else
+                return binarySearch(nums, target, mid+1, high);
+        }
+        else{
+            if(nums[mid] <= target && target <= nums[high])
+                return binarySearch(nums, target, mid + 1, high);
+            else
+                return binarySearch(nums, target, low, mid - 1);
+        }
+    }
     int search(vector<int>& nums, int target) {
         
         int low = 0, high = nums.size() - 1;
-        while(low <= high){
-            int mid = low + (high - low)/2;
+        return binarySearch(nums, target, low, high);
 
-            if(nums[mid] == target)
-                return mid;
+        // while(low <= high){
+        //     int mid = low + (high - low)/2;
+
+        //     if(nums[mid] == target)
+        //         return mid;
             
-            // check if array from low to mid is sorted
-            if(nums[low] <= nums[mid]){
-                // check if target is in this half
-                if(nums[low] <= target && target <= nums[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            }
-            // second half is sorted
-            else{
-                // check if target is in this half
-                if(nums[mid] <= target && target <= nums[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
-            }
-        }
-        return -1;
+        //     // check if array from low to mid is sorted
+        //     if(nums[low] <= nums[mid]){
+        //         // check if target is in this half
+        //         if(nums[low] <= target && target <= nums[mid])
+        //             high = mid - 1;
+        //         else
+        //             low = mid + 1;
+        //     }
+        //     // second half is sorted
+        //     else{
+        //         // check if target is in this half
+        //         if(nums[mid] <= target && target <= nums[high])
+        //             low = mid + 1;
+        //         else
+        //             high = mid - 1;
+        //     }
+        // }
+        // return -1;
     }
 };
