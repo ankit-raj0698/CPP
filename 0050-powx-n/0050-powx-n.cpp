fa550fff0c9ce8331d38a42ff39db7b4d543763b
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-09 12:48:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.67%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,19 +4,24 @@ public:
         if(n == 0)
             return 1;
         
+        // handle negative exponent 
         if(n < 0){
             x = 1/x;
             n = -n;
         }
 
-        double half = myPow(x,n/2);
+        // double half = binPow(x,n/2);
         
-        if(n % 2 == 0)   
-            return half * half;
+        if(n % 2 == 0){
+            double temp = binPow(x, n/2);
+            return temp * temp;
+        }   
+            
         else
-            return x * half * half;
+            return x * binPow(x, n - 1);
     }
     double myPow(double x, int n) {
+        //handle int overflow for exponent
         return binPow(x,(long)n);
     }
 };
