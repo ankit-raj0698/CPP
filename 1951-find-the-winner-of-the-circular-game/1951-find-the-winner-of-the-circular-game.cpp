db2aa6fe9e580498b4e023c9258dce599b8c72bc
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-11 22:35:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (81.9%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int solve(int n, int k){
         if(n == 1)
-            return 0;
+            return 0; // when only element left
         
         int ind = solve(n - 1, k);
-        ind = (ind + k) % n;
+        ind = (ind + k) % n; // to find the index of the winner after deletion
 
         return ind;
     }
     int findTheWinner(int n, int k) {
         int ind = solve(n, k);
-        return ind + 1;
+        return ind + 1; // as we have 0 based indexing
     }
 };
