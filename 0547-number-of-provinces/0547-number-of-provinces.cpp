cb2a6f1fc25e384a1288def09fff669072950966
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-13 14:02:48 +0530

    Time: 0 ms (100%), Space: 19.4 MB (73.58%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,84 +1,22 @@
 class Solution {
 public:
-    
-    void dfs(vector<vector<int>>& isConnected, int u, vector<bool> &visited,int n){
-        visited[u] = true;
-        // find the neighbours of u, here u => row so traverse all the column for row u
-        for(int v = 0; v < n ; v++){
-            if(!visited[v] && isConnected[u][v] == 1){
-                dfs(isConnected, v , visited, n);
-            }
+    void dfs(vector<vector<int>>& isConnected, vector<int> &visited, int u){
+        visited[u] = 1;
+        for(int v = 0; v < isConnected.size(); v++){
+            if(isConnected[u][v] && !visited[v])
+                dfs(isConnected, visited, v);
         }
     }
-    
-    void bfs(vector<vector<int>>& isConnected, int curr, vector<bool> &visited,int n){
-        queue<int> q;
-        
-        q.push(curr);
-        visited[curr] = true;
-        
-        while(!q.empty()){
-            int u = q.front();
-            q.pop();
-            
-            for(int v = 0; v < n; v++){
-                if(!visited[v] && isConnected[u][v] == 1){
-                    q.push(v);
-                    visited[v] = true;
-                }
-            }
-        }
-    }
-    
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n,false); 
-            
-        int count = 0;
+        vector<int> visited(n,0);
+        int cnt = 0;
         for(int i = 0; i < n ; i++){
             if(!visited[i]){
-               //dfs(isConnected, i, visited,n);
-                bfs(isConnected, i, visited,n);
-                count++;
+                dfs(isConnected, visited, i);
+                cnt++;
             }
         }
-      return count;  
+        return cnt;
     }
-    
-    /*
-    // using custom created adjacency list
-    void dfs(unordered_map<int,vector<int>> adj, int u, vector<bool> &visited){
-        visited[u] = true;
-        for(int v : adj[u]){
-            if(!visited[v]){
-                dfs(adj, v , visited);
-            }
-        }
-    } 
-    
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        unordered_map<int,vector<int>> adj;
-        vector<bool> visited(n,false); 
-            
-        for(int i = 0; i < n; i++){
-            for(int j = 0 ; j < n ; j++){
-               if(isConnected[i][j] == 1){
-                   adj[i].push_back(j);
-                   adj[j].push_back(i);
-               } 
-            }       
-        }
-        
-        int count = 0;
-        for(int i = 0; i < n ; i++){
-            if(!visited[i]){
-               dfs(adj, i, visited);
-                count++;
-            }
-        }
-      return count;  
-    }
-    */
-    
 };
