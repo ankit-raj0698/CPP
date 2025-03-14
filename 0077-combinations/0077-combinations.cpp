684b5eb11911733b54d5cfff40503c020e863587
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-15 00:11:25 +0530

    Time: 113 ms (18.77%), Space: 131.3 MB (18.65%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void solve(int start, int n, int k, vector<int> &temp, vector<vector<int>> &res){
-        
-        
+    vector<vector<int>> res;
+    void solve(int start, int n, int k, vector<int> &temp){
 
+        // when we got k elements in our temp ans
         if(k == 0){
             res.push_back(temp);
             return;
         }
 
+        // we have explored all n elements
         if(start > n)
             return;
 
-        solve(start + 1, n, k, temp, res);
+        // exclude the current element
+        solve(start + 1, n, k, temp);
+
+        // include the current element
         temp.push_back(start);
-        solve(start + 1, n, k - 1, temp, res);
+        solve(start + 1, n, k - 1, temp);
         temp.pop_back();
     }
     vector<vector<int>> combine(int n, int k) {
-        int start = 1;
         vector<int> temp;
-        vector<vector<int>> res;
 
-        solve(start, n, k, temp, res);
+        solve(1, n, k, temp);
 
         return res;
         
