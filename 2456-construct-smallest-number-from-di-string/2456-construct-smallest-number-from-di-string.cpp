dd3f68148ea1c01ce46a5b1576fc76353310bd22
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-14 22:19:46 +0530

    Time: 0 ms (100%), Space: 8 MB (63.04%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool matches(string num, string pattern){
-        for(int i = 0; i < pattern.size(); i++){
-            if((pattern[i] == 'I' && num[i] > num[i+1]) || (pattern[i] == 'D' && num[i] < num[i+1]))
-                return false;
-        }
-        return true;
-    }
     string smallestNumber(string pattern) {
-        string str = "";
-        for(int i = 1; i <= pattern.size() + 1; i++)
-            str.push_back(i+'0');
-            
-        while(!matches(str,pattern)){
-            next_permutation(str.begin(), str.end());
-        }
+        int count = 0;
+        stack<int> st;
+        string ans = "";
+        for(int i = 0; i <= pattern.size(); i++){
 
-        return str;
+            count++;
+            if(pattern[i] == 'I' || i == pattern.size()){
+                ans.push_back(count + '0');
+                while(!st.empty()){
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+            }
+            else if(pattern[i] == 'D'){
+                st.push(count + '0');
+            }
+        }
+        return ans;
     }
 };
