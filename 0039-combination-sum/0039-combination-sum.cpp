a03b9ea90abd59c52c00e25cc31c996da1e180f1
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-15 00:25:25 +0530

    Time: 5 ms (35.75%), Space: 18 MB (28.87%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void solve(vector<int>& candidates, int target, int ind, vector<int> &temp){
+        if(target == 0){
+            res.push_back(temp);
+            return;
+        }
+        if(target < 0 || ind == candidates.size())
+            return;
+        
+        // exclude the current element
+        solve(candidates, target, ind + 1, temp);
+        // include the current element
+        temp.push_back(candidates[ind]);
+        solve(candidates, target - candidates[ind], ind, temp);
+        temp.pop_back();
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        solve(candidates, target, 0, temp);
+        return res;
+    }
+};
