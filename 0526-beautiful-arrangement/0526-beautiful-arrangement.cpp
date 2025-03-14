f80728e89cb88a0b062cf79a3959f9df88310004
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-14 12:08:32 +0530

    Time: 39 ms (55.9%), Space: 7.9 MB (57.53%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<int> &nums, int l, int r, int &count){
+
+        if(l == nums.size()){
+            count++;
+            return;
+        }
+
+        for(int i = l; i <= r; i++){
+            swap(nums[l], nums[i]);
+            if(nums[l] %( l+1) == 0 || (l + 1) % nums[l] == 0)
+                solve(nums, l + 1, r, count);
+            swap(nums[l], nums[i]);
+        }
+    }
+    int countArrangement(int n) {
+        int count = 0;
+        vector<int> nums(n,0);
+        for(int i = 0; i < n; i++)
+            nums[i] = i + 1;
+        
+        solve(nums, 0, n-1, count);
+        return count;
+        
+    }
+};
