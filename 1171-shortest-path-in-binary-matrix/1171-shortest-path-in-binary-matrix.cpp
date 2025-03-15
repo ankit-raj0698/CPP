848aa3625d27ed0b9907dc18a50aa345f83feb35
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-16 02:19:35 +0530

    Time: 26 ms (44.83%), Space: 23.7 MB (79.24%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,41 +2,41 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         
-        typedef pair<int,int> t;
+        typedef pair<int,pair<int,int>> t;
         int n = grid.size();
-        vector<vector<int>> visited(n, vector<int>(n,0));
-        vector<t> direction = {{0,1}, {0,-1}, {1,0}, {-1,0},
+        vector<vector<int>> dist(n, vector<int>(n,1e9));
+        vector<pair<int,int>> direction = {{0,1}, {0,-1}, {1,0}, {-1,0},
                                 {1,1}, {1,-1}, {-1,1}, {-1,-1}};
             
-        queue<t> q;
+        priority_queue<t, vector<t>, greater<t>> pq;
 
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
 
-        q.push({0,0});
-        visited[0][0] = 1;
+        // we are also considering cell (0,0) for dist
+        pq.push({1,{0,0}});
+        dist[0][0] = 1;
 
-        int level = 1;
-        while(!q.empty()){
-            int size = q.size();
-            while(size--){
-                auto [i, j] = q.front();
-                q.pop();
+        while(!pq.empty()){
+            
+            int d = pq.top().first;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            pq.pop();
 
-                if(i == n-1 && j == n-1)
-                    return level;
+            if(i == n-1 && j == n-1)
+                return dist[i][j];
                 
-                for(auto dir: direction){
-                    int row = i + dir.first;
-                    int col = j + dir.second;
+            for(auto dir: direction){
+                int row = i + dir.first;
+                int col = j + dir.second;
 
-                    if(row >= 0 && row < n && col >= 0 && col < n && !visited[row][col] && grid[row][col] == 0){
-                        q.push({row, col});
-                        visited[row][col] = 1;
+                if(row >= 0 && row < n && col >= 0 && col < n && grid[row][col] == 0 && (d + 1 < dist[row][col])){
+                    dist[row][col] = d + 1;
+                    pq.push({dist[row][col] ,{row, col}});
+                    
                     }
                 }
-            }
-            level++;
         }
         return -1;
     }
