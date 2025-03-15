e2d280fe117ec972a387e6a2a87dbf712d71b118
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-15 17:58:04 +0530

    Time: 203 ms (27.53%), Space: 135.9 MB (22.03%) - LeetHub

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(unordered_map<int, vector<pair<int,int>>> &adj, vector<int> &visited, int u, int &count){
+        visited[u] = 1;
+        for(auto p: adj[u]){
+            int v = p.first;
+            int direction = p.second;
+            if(!visited[v]){
+                if(direction == 0)
+                    count++;
+                dfs(adj, visited, v, count);
+            }
+        }
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        unordered_map<int, vector<pair<int,int>>> adj;
+        vector<int> visited(n, 0);
+
+        for(auto vec: connections){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back({v,0});
+            adj[v].push_back({u,1});
+        }
+
+        int count = 0;
+        dfs(adj, visited, 0, count);
+        return count;
+        
+    }
+};
