5ce52fbdace1d4ce5c47a782c45402909fe455c7
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-16 02:06:36 +0530

    Time: 27 ms (43.88%), Space: 25.4 MB (57.29%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        typedef pair<int,int> t;
         int n = grid.size();
         vector<vector<int>> visited(n, vector<int>(n,0));
-        queue<pair<int,int>> q;
-        vector<pair<int,int>> direction = {{1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+        vector<t> direction = {{0,1}, {0,-1}, {1,0}, {-1,0},
+                                {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+            
+        queue<t> q;
 
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
-        
+
         q.push({0,0});
         visited[0][0] = 1;
 
@@ -16,7 +20,7 @@ public:
         while(!q.empty()){
             int size = q.size();
             while(size--){
-                auto [i,j] = q.front();
+                auto [i, j] = q.front();
                 q.pop();
 
                 if(i == n-1 && j == n-1)
@@ -27,11 +31,10 @@ public:
                     int col = j + dir.second;
 
                     if(row >= 0 && row < n && col >= 0 && col < n && !visited[row][col] && grid[row][col] == 0){
-                        visited[row][col] = 1;
                         q.push({row, col});
+                        visited[row][col] = 1;
                     }
                 }
-
             }
             level++;
         }
