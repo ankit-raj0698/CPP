1ab90e84f1eaa3c4eb9f400265fd97998d820825
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-15 17:34:01 +0530

    Time: 8 ms (6.12%), Space: 16.6 MB (5.01%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // standard DFS code
     void dfs(unordered_map<int, vector<int>> &adj, vector<int> &visited, int u){
         visited[u] = 1;
         for(int v: adj[u]){
@@ -11,19 +12,21 @@ public:
         unordered_map<int, vector<int>> adj;
         int n = rooms.size();
 
+        // create the adjacency list
         for(int u = 0; u < n; u++){
             for(int v: rooms[u]){
                 adj[u].push_back(v);
             }
         }
 
+        // start the dfs from room no 0 and count no. of components in graph
         vector<int> visited(n, 0);
-        int count = 0;
+        int noOfComponent = 0;
         for(int i = 0; i < n; i++){
             if(!visited[i]){
                 dfs(adj, visited, i);
-                count++;
-                if(count > 1)
+                noOfComponent++;
+                if(noOfComponent > 1)
                     return false;
             }
         }
