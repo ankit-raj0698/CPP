e6069dd1125f31fb4b60dbb27b6216f7142544c9
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-16 00:43:28 +0530

    Time: 102 ms (16.28%), Space: 47.8 MB (32.62%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -3,6 +3,8 @@ public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         typedef pair<int,int> t;
         unordered_map<int, vector<t>> adj;
+
+        // create the adjacency matrix
         for(auto vec: times){
             int u = vec[0];
             int v = vec[1];
@@ -13,25 +15,30 @@ public:
 
         priority_queue<t, vector<t>, greater<t>> pq;
         vector<int> dist(n+1, 1e9);
+
+        // push the dist, src in priority_queue
         pq.push({0,k});
         dist[k] = 0;
-        
 
         while(!pq.empty()){
             auto [du, u] = pq.top();
             pq.pop();
 
-            for(auto [v,wt] : adj[u]){
+            for(auto [v,wt]: adj[u]){
                 if(du + wt < dist[v]){
                     dist[v] = du + wt;
                     pq.push({dist[v], v});
                 }
             }
         }
+
         int ans = -1;
         for(int i = 1; i <= n; i++){
             ans = max(ans, dist[i]);
         }
+
         return ans == 1e9 ? -1 : ans;
+
+        
     }
 };
