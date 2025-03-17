6351a7bbd2e7dc8dff453d95f5adac542b1a3c7b
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-17 06:53:12 +0530

    Time: 8 ms (77.06%), Space: 52.4 MB (71.11%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-    // when you closely look the example you will realise that all the states which are part of cycle are not safe
-    bool dfsCycle(vector<vector<int>>& graph, int u, vector<int> &visited, vector<int> &inRecursion){
+    bool dfs(vector<vector<int>>& graph, vector<int> &visited, vector<int> &inRecursion, int u){
         visited[u] = 1;
         inRecursion[u] = 1;
 
         for(int v: graph[u]){
             if(!visited[v]){
-                if(dfsCycle(graph, v, visited, inRecursion))
+                if(dfs(graph, visited, inRecursion, v))
                     return true;
             }
             else if(inRecursion[v])
                 return true;
         }
-
-
         inRecursion[u] = 0;
         return false;
-
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // those nodes are safe which are not part of cycle
+        // using DFS we can find the nodes which are part of cycle 
+        // inRecursion vector will tell us about the nodes which are part of cycle 
 
         int n = graph.size();
-        vector<int> visited(n,0);
-        vector<int> inRecursion(n,0);
+        vector<int> visited(n,0) , inRecursion(n, 0);
 
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                dfsCycle(graph, i, visited, inRecursion);
+                // call DFS
+                dfs(graph, visited, inRecursion, i);
             }
         }
 
-        // states which are not part of cycle are safe states
-        // if inRecursion of a node is false it means it is not part of cycle
-        vector<int> safeState; 
+        vector<int> ans;
         for(int i = 0; i < n; i++){
-            if(inRecursion[i] == 0)
-                safeState.push_back(i);
+            if(inRecursion[i] == 0){
+                // ith node is not part of cycle
+                ans.push_back(i);
+            }
         }
-        
-        return safeState;
-        
+        return ans;
     }
 };
