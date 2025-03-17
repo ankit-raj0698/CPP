412ba2100d7e17458007b287926e1eec70741360
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-17 06:13:56 +0530

    Time: 5 ms (47.9%), Space: 18.7 MB (42.3%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,45 +1,49 @@
 class Solution {
 public:
-
-    // if topological sort is possible then ans true otherwise {}
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        // map to store adjacency list
-        unordered_map<int, vector<int>> adj;
         vector<int> indegree(numCourses, 0);
+        vector<vector<int>> adj(numCourses);
 
-        vector<int> ans;
-        queue<int> q;
-
-        for(auto &vec: prerequisites){
+        // create the adjacency list
+        for(auto vec: prerequisites){
             int u = vec[0];
             int v = vec[1];
-            // [ai, bi] => bi -> ai
+            // v --> u
             adj[v].push_back(u);
-            indegree[u]++;
         }
 
-        // push all the nodes in queue whose indegree is 0
-        for(int i = 0; i < numCourses; i++){
+        // find the indegree of each vertex
+        for(int u = 0; u < numCourses; u++){
+            for(auto v : adj[u])
+                indegree[v]++;
+        }
+
+        queue<int> q;
+        // push the vertices whose indegree is 0
+        for(int i = 0; i < indegree.size(); i++){
             if(indegree[i] == 0)
                 q.push(i);
         }
 
+        vector<int> ans;
         while(!q.empty()){
             int u = q.front();
             q.pop();
             ans.push_back(u);
 
-            // decrease the indegree of adj[u]
+            // remove the adjacent edges of u i.e decrease the indegree
             for(int v: adj[u]){
                 indegree[v]--;
                 if(indegree[v] == 0)
                     q.push(v);
             }
         }
-        // when topological sort is possible
-        if(ans.size() == numCourses)
-            return ans;
+
+        // if we don't have all the courses in ans, it means topo sort not possible
+        if(ans.size() != numCourses)
+            return {};
         
-        return {};
+        return ans;
+        
     }
 };
