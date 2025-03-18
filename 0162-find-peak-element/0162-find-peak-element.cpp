004e07062a65b6c90817970e59ee0a7db1d544b6
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-18 07:06:15 +0530

    Time: 0 ms (100%), Space: 12.5 MB (67.79%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int n;
-    bool check(vector<int> &nums, int mid){
-        if(mid == n - 1)
+    bool check(vector<int>& nums,int mid){
+        // if mid is the last element of array, then it is peak
+        if(mid == nums.size()-1)
             return true;
+        
         if(nums[mid] > nums[mid + 1])
             return true;
+        
         return false;
     }
     int findPeakElement(vector<int>& nums) {
-        n = nums.size();
-        int low = 0, high = n-1;
-
+        int low = 0, high = nums.size() - 1;
         int ans = -1;
+
         while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(check(nums, mid)){
+            int mid = low + (high - low)/2;
+            if(check(nums,mid)){
                 ans = mid;
-                high = mid - 1;
+                high = mid -1;
             }
-            else 
+            else
                 low = mid + 1;
         }
         return ans;
