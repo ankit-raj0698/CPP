c746d441bfe3be62a8ff0373a1f00a0ded03dd94
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-18 07:18:15 +0530

    Time: 0 ms (100%), Space: 17.4 MB (86.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,56 +1,61 @@
 class Solution {
-    
 public:
-    
+    bool leftCheck(vector<int>& nums, int target, int mid){
+        if(nums[mid] >= target)
+            return true;
+        return false;
+    }
+
+    bool rightCheck(vector<int>& nums, int target, int mid){
+        if(nums[mid] <= target)
+            return true;
+        return false;
+    }
+
     int leftOccurrence(vector<int>& nums, int target){
         int start=0;
         int end=nums.size()-1;
         int ans=-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                end=mid-1;
+            if(leftCheck(nums, target, mid)){
+                ans = mid;
+                end = mid - 1;
             }
-            
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
+            else
+                start = mid + 1;
         }
+        if(ans != -1 && nums[ans] != target)
+            return -1;
+
         return ans;
     }
-    
+
     int rightOccurrence(vector<int>& nums, int target){
         int start=0;
         int end=nums.size()-1;
         int ans=-1;
         while(start<=end){
             int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                ans=mid;
-                start=mid+1;
-            }        
-            else if(nums[mid]<target){
-                start=mid+1;
+            if(rightCheck(nums, target, mid)){
+                ans = mid;
+                start = mid + 1;
             }
-            else{
-                end=mid-1;
-            }
+            else
+                end = mid - 1;
         }
+        if(ans != -1 && nums[ans] != target)
+            return -1;
+            
         return ans;
     }
-    
+
     vector<int> searchRange(vector<int>& nums, int target) {
-        
         vector<int> ans;
         int left=leftOccurrence(nums,target);
         int right=rightOccurrence(nums,target);
         ans.push_back(left);
         ans.push_back(right);
         return ans;
-        
     }
 };
