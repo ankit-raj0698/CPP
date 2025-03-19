7300f664f3cf7026234090ed50500069f8dc7084
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-20 00:11:20 +0530

    Time: 29 ms (55.66%), Space: 13.4 MB (95.36%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[13][10001];
+    int solve(vector<int>& coins, int amount, int ind){
+        if(ind == 0){
+            if(amount % coins[ind] == 0)
+                return amount / coins[ind];
+            return 1e9;
+        }
+
+        if(dp[ind][amount] != -1)
+            return dp[ind][amount];
+        
+        int not_pick = solve(coins, amount, ind - 1);
+        int pick = 1e9;
+        if(coins[ind] <= amount)
+            pick = 1 + solve(coins, amount - coins[ind], ind);
+        
+        return dp[ind][amount] = min(pick,not_pick);
+        
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        memset(dp, -1, sizeof(dp));
+        int ans = solve(coins, amount, n-1);
+
+        return ans == 1e9 ? -1 : ans;
+    }
+};
