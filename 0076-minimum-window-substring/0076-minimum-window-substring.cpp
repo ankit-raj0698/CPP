c2b1bd1071fa0e74a26ade5df7fb2fa7847e7753
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-26 01:20:13 +0530

    Time: 7 ms (65.72%), Space: 11.5 MB (42.34%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,47 +1,47 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        string ans  = "";
-        unordered_map<char,int> mp;
-        
         if(t.size() > s.size())
-            return ans;
+            return "";
         
-        for(auto ele:t){
-            mp[ele]++;
-        }
+        unordered_map<char,int> mp;
+        for(char ch: t)
+            mp[ch]++;
         
-        int count = mp.size();
-        int left = 0, right = 0;
-        int minlen = INT_MAX,  leftindex = -1;
-        
+        int cnt = t.size(); // size of window
+        int left = 0, right = 0, minlen = 1e9, start = -1;
+
         while(right < s.size()){
-            if(mp.find(s[right]) != mp.end()){
-                mp[s[right]]--;
-                if(mp[s[right]] == 0)
-                    count--;
-            }
+            char ch = s[right];
+
+            // char which are present in t only those count > 0
+            // so we have got 1 reqd char
+            if(mp[ch] > 0)
+                cnt--;
             
-            
-            while(count == 0){
+            mp[ch]--;
+
+
+            while(cnt == 0){
+                // when curr window size < min window size
+                // update the window size and track from where this window is starting
                 if(right - left + 1 < minlen){
-                    leftindex = left;
                     minlen = right - left + 1;
+                    start = left;
                 }
-                
-                if(mp.find(s[left]) != mp.end()){
-                    mp[s[left]]++;
-                    if(mp[s[left]] == 1)
-                        count++;
-                } 
+
+                // try to shrink the current window from left side
+                mp[s[left]]++;
+
+                // only case where we get count > 0 in map
+                // when this is a reqd char
+                if(mp[s[left]] > 0)
+                    cnt++;
                 left++;
             }
-          right++;  
+            right++;
         }
-        if(leftindex == -1)
-            return "";
-        else{
-            return s.substr(leftindex,minlen);
-        } 
+
+        return start == -1 ? "" : s.substr(start, minlen);
     }
 };
