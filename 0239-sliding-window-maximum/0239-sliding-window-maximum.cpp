ce26f658a5ac4e30d30d43db0f89a6ae5bfc7ad2
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-26 01:32:42 +0530

    Time: 173 ms (13.27%), Space: 198.6 MB (6.53%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,32 +2,34 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         
-        int left = 0, right = 0;
-        list<int> l;
         vector<int> ans;
-        
+
+        // to track the max in window
+        list<int> l;
+
+        int left = 0, right = 0;
         while(right < nums.size()){
-            //calculations
-           // say currently we are at index right so all the no smaller than s[right] 
-            // from back of list should be deleted
-                while(!l.empty() && l.back() < nums[right]){
-                    l.pop_back();
-                }
-                l.push_back(nums[right]);
-            
-            
-            //window size == k
+
+            // when we encounter a greater no in the current window
+            // then all the prev smaller no are useless
+            // but smaller no after curr element can be useful for other window
+            while(!l.empty() && l.back() < nums[right])
+                l.pop_back();
+            l.push_back(nums[right]);
+
             if(right - left + 1 == k){
+                // store the answer
                 ans.push_back(l.front());
-            
-            //slide the window
-            if(l.front() == nums[left]){
-                l.pop_front();
+
+                // shrink the window from left side
+                // since we have to maintain size of k, just check for nums[left]
+                if(l.front() == nums[left])
+                    l.pop_front();
+                
+                left++;               
             }
-            left++;
-            }         
-            right++;            
+            right++;
         }
-      return ans;  
+        return ans;
     }
 };
