942417f22ec576c82b123f0638598180d61d69f3
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-25 15:57:44 +0530

    Time: 118 ms (61.02%), Space: 10.9 MB (99.42%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -3,31 +3,22 @@ public:
     int dp[301][301];
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, 0, sizeof(dp));
+        memset(dp, -1, sizeof(dp));
 
-        // l denotes length
-        for(int l = 1; l <=n; l++){
-            // i goes from to 0 to last valid row
+        for(int l = 1; l <= n; l++){
             for(int i = 0; i <= n-l; i++){
                 // l = j - i + 1
-                int j = i  + l - 1;
-                // k can be anything from i to j
-                for(int k = i; k <=j; k++){
-                    // when k == i then dp[i][i-1] which is invalid so handle it
+                int j = l + i - 1;
+
+                for(int k = i; k <= j; k++){
                     int left = i == k ? 0 : dp[i][k-1];
-                    // when k == j then dp[j+1][j] which is invalid so handle it
                     int right = j == k ? 0 : dp[k+1][j];
 
-                    // handle out of bound case for i and j
-                    int val = (i == 0 ? 1 :nums[i-1]) * nums[k] * (j == n-1 ? 1 : nums[j+1]);
-
+                    int val = (i == 0 ? 1 : nums[i-1]) * nums[k] * (j == n - 1 ? 1:  nums[j+1]);
                     dp[i][j] = max(dp[i][j], left + val + right);
                 }
             }
         }
-
-// return first row last col val
         return dp[0][n-1];
-        
     }
 };
