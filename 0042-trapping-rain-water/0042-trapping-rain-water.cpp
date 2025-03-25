2e4aded980f81573982039841b9fcb17cb7f2543
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-26 00:05:09 +0530

    Time: 0 ms (100%), Space: 27.2 MB (39.1%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,93 +1,32 @@
 class Solution {
 public:
-    /*
-    two pointer approach
-    iterate the loop while (left < right)
-    maintain two variable leftmax and rightmax
-    initialize leftmax = leftmost element of input array
-    initialize rightmax = rightmost element of input array
-    whichever is smaller among leftmax and rightmax update it
-    1. if leftmax is smaller than rightmax do left++ otherwise right--
-    2. tempWaterTrap = leftmax or rightmax - height[left] or height[right] acc to updation in prev step
-    3. if tempWaterTrap < 0 , do tempWaterTrap = 0
-    store tempWaterTrap in ans
-    4. update leftmax and rightmax accordingly    
-    */
-    
-    int trap(vector<int>& height) {
-        
-        int left = 0;
-        int right = height.size()-1;
-        
-        int leftmax = height[left];
-        int rightmax = height[right];
-        
-        int ans = 0;
-        
-        while(left < right){
-           
-            if(leftmax < rightmax){
-            left++;
-            int res = leftmax - height[left];
-            
-            if(res < 0)
-                res = 0;
-                
-            ans+=res;
-            leftmax = max(leftmax,height[left]);
-        }
-        else{
-            right--;
-            int res = rightmax - height[right];
-            
-            if(res < 0)
-                res = 0;
-            
-            ans+=res;
-            rightmax = max(rightmax,height[right]);
-        }            
-    }      
-        return ans;
-    }
-    
-    
-    /*
-    solution with time complexity O(n) and space complexity O(n)
-    let total water to be trapped be ans = 0;
-    create two vectors leftmax and rightmax of size n
-    leftmax[i] --> max of all the elemments to the left of i
-    rightmax[i] --> max of all the elemments to the right of i
-    LOGIC : res = min(leftmax[i],rightmax[i]) - height[i]
-    if res < 0 then make res = 0
-    ans = ans + res
-    */
-    
-    /*
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> leftmax(n);
-        vector<int> rightmax(n);
-        
-        leftmax[0] = 0;
-        rightmax[n-1] = 0;
-        
-        for(int i=1; i<n; i++){
-            leftmax[i] = max(leftmax[i-1],height[i-1]);
+
+        vector<int> left(n); 
+        vector<int> right(n);
+        int leftmax = height[0];
+        int rightmax = height[n-1];
+
+        for(int i = 0; i < n; i++){
+            left[i] = leftmax;
+            leftmax = max(leftmax, height[i]);
+        } 
+
+        for(int i = n-1; i >= 0; i--){
+            right[i] = rightmax;
+            rightmax = max(rightmax, height[i]);
         }
-        
-        for(int i=n-2; i>=0; i--){
-            rightmax[i] = max(rightmax[i+1],height[i+1]);
+            
+        int total = 0;
+        for(int i = 0; i < n; i++){
+            
+            int waterTrapped = min(left[i], right[i]) - height[i];
+            // when waterTrapped < 0, then add 0 to total
+            total += waterTrapped < 0 ? 0 : waterTrapped;
+            
         }
-        
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            int res = min(leftmax[i],rightmax[i]) - height[i];
-            if(res < 0){
-                res = 0;
-            }
-            ans+=res;
-        }
-        return ans;
+
+         return total;   
     }
-    */
 };
