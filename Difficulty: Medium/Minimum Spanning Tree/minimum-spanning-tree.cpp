d88c7882d84104fcad509bb5a2e0a7d752b9bcbb
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-27 17:59:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,61 +1,62 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
-class Solution
-{
-	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
+
+class Solution {
+  public:
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
         // code here
-        typedef pair<int,int> t;
-        priority_queue<t,vector<t>,greater<t>> pq;
-        vector<bool> visited(V,false);
-        int sum = 0;
         
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> visited(V,0);
+        
+        int sum = 0;
         pq.push({0,0});
         
         while(!pq.empty()){
-            int d = pq.top().first;
-            int node = pq.top().second;
+            //auto [du, u] = pq.top();
+            pair<int,int> temp = pq.top();
+            int du = temp.first;
+            int u = temp.second;
             pq.pop();
             
-            if(!visited[node]){
-                visited[node] = true;
-                sum += d;
-                
-                for(auto &vec: adj[node]){
-                    int neighbour = vec[0];
-                    int neighbour_wt = vec[1];
-                    
-                    if(!visited[neighbour])
-                        pq.push({neighbour_wt,neighbour});
+            if(visited[u])
+                continue;
+            sum += du;
+            visited[u] = 1;
+            
+            for(auto p: adj[u]){
+                int v = p[0];
+                int wt = p[1];
+                if(!visited[v]){
+                    pq.push({wt, v});
                 }
-                
             }
         }
+        
         return sum;
     }
 };
 
+
 //{ Driver Code Starts.
 
-
-int main()
-{
+int main() {
     int t;
     cin >> t;
     while (t--) {
         int V, E;
         cin >> V >> E;
         vector<vector<int>> adj[V];
-        int i=0;
-        while (i++<E) {
+        int i = 0;
+        while (i++ < E) {
             int u, v, w;
             cin >> u >> v >> w;
-            vector<int> t1,t2;
+            vector<int> t1, t2;
             t1.push_back(v);
             t1.push_back(w);
             adj[u].push_back(t1);
@@ -63,13 +64,15 @@ int main()
             t2.push_back(w);
             adj[v].push_back(t2);
         }
-        
+
         Solution obj;
-    	cout << obj.spanningTree(V, adj) << "\n";
+        cout << obj.spanningTree(V, adj) << "\n";
+
+        cout << "~"
+             << "\n";
     }
 
     return 0;
 }
-
 
 // } Driver Code Ends
