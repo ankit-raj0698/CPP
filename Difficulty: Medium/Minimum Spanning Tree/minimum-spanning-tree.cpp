c1e5d8e58970eb3c34fbad41589df0c13f69293e
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-27 20:55:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -8,37 +8,80 @@ using namespace std;
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    vector<int> parent;
+    vector<int> rank;
+    
+    struct comparator{
+        bool operator()(vector<int> &v1, vector<int> &v2){
+            return v1[2] < v2[2];
+        }
+    };
+    
+    int find(int x){
+        if(x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+    
+    void dsu(int u, int v){
+        int parent_u = find(u);
+        int parent_v = find(v);
+        
+        if(parent_u != parent_v){
+            if(rank[parent_u] > rank[parent_v])
+                parent[parent_v] = parent_u;
+            else if(rank[parent_v] > rank[parent_u])
+                parent[parent_u] = parent_v;
+            else{
+                parent[parent_u] = parent_v;
+                rank[parent_v]++;
+            }
+        }
+    }
+    
+    int kruskals(vector<vector<int>> &vec){
+        int sum = 0;
+        for(auto temp: vec){
+            int u = temp[0];
+            int v = temp[1];
+            int wt = temp[2];
+            
+            int parent_u = find(u);
+            int parent_v = find(v);
+            
+            // u, v are in different component so do a union of them
+            if(parent_u != parent_v){
+                dsu(u,v);
+                sum += wt;
+            }
+        }
+        return sum;
+    }
+    
     int spanningTree(int V, vector<vector<int>> adj[]) {
         // code here
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        vector<int> visited(V,0);
+        // store u,v,wt from adj list in a vector so that we can sort acc to edge wt
+        vector<vector<int>> vec;
+        parent.resize(V);
+        rank.resize(V,0);
         
-        int sum = 0;
-        pq.push({0,0});
+        for(int i = 0; i < V; i++)
+            parent[i] = i;
         
-        while(!pq.empty()){
-            //auto [du, u] = pq.top();
-            pair<int,int> temp = pq.top();
-            int du = temp.first;
-            int u = temp.second;
-            pq.pop();
-            
-            if(visited[u])
-                continue;
-            sum += du;
-            visited[u] = 1;
-            
-            for(auto p: adj[u]){
-                int v = p[0];
-                int wt = p[1];
-                if(!visited[v]){
-                    pq.push({wt, v});
-                }
+        for(int u = 0; u < V; u++){
+            for(auto temp: adj[u]){
+                int v = temp[0];
+                int wt = temp[1];
+                
+                vec.push_back({u, v, wt});
             }
         }
+        // sort the edges acc to wt
+        sort(vec.begin(), vec.end(), comparator());
         
-        return sum;
+        // call kruskals
+        return kruskals(vec);
     }
 };
 
