c11746c9eb2bb206464aacf5072f66b4bfdbcfc8
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-03-28 09:58:08 +0530

    Time: 3 ms (41.95%), Space: 14.3 MB (25.98%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     vector<vector<int>> res;
     void solve(vector<int>& nums, int target, int ind, vector<int> &temp){
+
         if(target == 0){
             res.push_back(temp);
             return;
         }
 
-        if(target < 0 || ind >= nums.size())
+        if(ind >= nums.size())
             return;
         
 
         for(int i = ind; i < nums.size(); i++){
-            // handle duplicate elements
-            if(i > ind && nums[i] == nums[i-1])
+
+            if(i > ind && nums[i] == nums[i - 1])
                 continue;
-            // include current element
+
             if(nums[i] <= target){
-                temp.push_back(nums[i]);
-                solve(nums, target - nums[i], i + 1, temp);
-                temp.pop_back();
-            }          
+            temp.push_back(nums[i]);
+            solve(nums, target - nums[i], i + 1, temp);
+            temp.pop_back();
+            }
         }
     }
 
