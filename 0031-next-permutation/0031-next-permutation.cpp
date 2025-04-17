1f282b34afb054897c4c62e585ee97b3f9619bee
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-04-17 23:44:19 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,17 +3,13 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int ind = -1;
-
-        // find the ind from right where nums[i] < nums[i+1]
-        for(int i = n-2; i >= 0; i--){
+        for(int i = n - 2; i >= 0; i--){
             if(nums[i] < nums[i+1]){
                 ind = i;
                 break;
             }
         }
 
-        // find the ind of the element from right which is greater than nums[ind]
-        // swap this with nums[ind]
         if(ind != -1){
             for(int i = n-1; i > ind; i--){
                 if(nums[i] > nums[ind]){
@@ -23,8 +19,7 @@ public:
             }
         }
 
-        // reverse the array from ind + 1 till end
-        reverse(nums.begin() + ind + 1, nums.end());
+       reverse(nums.begin() + ind + 1, nums.end());
         
     }
 };
