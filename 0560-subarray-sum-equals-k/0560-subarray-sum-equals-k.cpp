3eeb6f097e002a8763947f68790fe20859e22560
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-04-20 09:33:43 +0530

    Time: 39 ms (78.54%), Space: 45.5 MB (56.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,16 +3,21 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
         int ans = 0;
-        int sum = 0;
-        mp[sum]++;
+        int prefixSum = 0;
+        mp[prefixSum]++;
 
+        // we are following the prefix sum concept
+        // prefixsum[right] - prefixsum[left - 1] = k
+        // that is prefixsum[right] - k = prefixsum[left - 1]
+        // we are checking at each index ending at right
+        // how many occurrences of prefixsum[right] - k is present
         for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
+            prefixSum += nums[i];
 
-            if(mp.find(sum - k) != mp.end())
-                ans += mp[sum - k];
+            if(mp.find(prefixSum - k) != mp.end())
+                ans += mp[prefixSum - k];
             
-            mp[sum]++;
+            mp[prefixSum]++;
         }
         return ans;
     }
