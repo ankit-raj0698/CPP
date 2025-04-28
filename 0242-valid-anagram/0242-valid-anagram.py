0950a8c9ada667c237e49430c947563813684f71
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-04-28 09:30:34 +0530

    Time: 13 ms (49.9%), Space: 18 MB (49.55%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,35 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
+    '''
         cnt1 = Counter(s)
         cnt2 = Counter(t)
+        return cnt1 == cnt2 
+    '''
+    def isAnagram(self, s: str, t: str) -> bool:
+        mp = dict()
 
-        return cnt1 == cnt2
+        for c in s:
+            if c in mp:
+                mp[c] += 1
+            else: 
+                mp[c] = 1
+        
+        print(mp)
+        
+        for c in t:
+            if c in mp:
+                mp[c] -= 1
+            else:
+                return False
+        
+        print(mp)
+        
+        for c in mp:
+            if mp[c] == 0:
+                continue
+            else:
+                return False
+        
+        return True
+            
+        
         
