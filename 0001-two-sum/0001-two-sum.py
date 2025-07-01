402fb8b246ed59437b88fd8bb5b086497eb6cba7
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-07-01 23:22:18 +0530

    Time: 0 ms (100%), Space: 18.8 MB (62.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = dict()
+        lookup = {}
 
         for ind, ele in enumerate(nums):
             rem = target - ele
