a5a7ab5cdeeaa7f2092aa6d0cce37b0a7a77c21d
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-07-01 23:21:58 +0530

    Time: 0 ms (100%), Space: 18.9 MB (33.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {}
+        lookup = dict()
 
         for ind, ele in enumerate(nums):
             rem = target - ele
-            if rem in mp:
-                return [mp[rem], ind]
-            mp[ele] = ind
- 
-
+            if rem in lookup:
+                return [lookup[rem], ind]
+            
+            lookup[ele] = ind
+        
         return []
-
         
