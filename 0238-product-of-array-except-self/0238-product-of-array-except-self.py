d153b4ea90bffd44769133f1755adfd6524c96f2
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-07-02 23:40:46 +0530

    Time: 23 ms (63.88%), Space: 25.8 MB (33.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -7,12 +7,12 @@ class Solution:
         for i in range(1,n):
             left[i] = left[i-1] * nums[i-1]
         
-        for i in range(n-2,-1,-1):
-            right[i] = right[i+1] * nums[i+1]
         
         ans = [1] * n
-        for i in range(n):
-            ans[i] = left[i] * right[i]
+        prod = 1
+        for i in range(n-1, -1, -1):
+            ans[i] = left[i] * prod
+            prod *= nums[i]
         
         return ans
         
