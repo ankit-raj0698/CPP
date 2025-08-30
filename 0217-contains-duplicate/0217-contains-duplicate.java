b1fb1646876b7fd406a3554e540db18273ddbc17
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-08-30 14:31:49 +0530

    Time: 13 ms (88.81%), Space: 58.6 MB (39.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int ele: nums){
+            if(s.contains(ele)){
+                return true;
+            }
+            s.add(ele);
+        }
+        return false;
+    }
+}
