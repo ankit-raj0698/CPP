d0dbdbc157ccc410f71233f21718d8ef8e7838e7
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-09-01 21:36:12 +0530

    Time: 173 ms (14.21%), Space: 62.3 MB (34.79%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.java
@@ -2,21 +2,20 @@ import java.util.*;
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
         NavigableSet<Long> set = new TreeSet<>();
-
         for(int i = 0; i < nums.length; i++){
-            Long ele = (long) nums[i];
-            Long ceil = set.ceiling(ele);
+            Long ele = (long)nums[i];
             Long floor = set.floor(ele);
+            Long ceiling = set.ceiling(ele);
 
-            if(floor != null && Math.abs(floor - ele) <= valueDiff)
+            if(floor != null && Math.abs(ele - floor) <= valueDiff)
                 return true;
-
-            if(ceil != null && Math.abs(ceil - ele) <= valueDiff)
+            
+            if(ceiling != null && Math.abs(ele - ceiling) <= valueDiff)
                 return true;
             
             set.add(ele);
             if(set.size() > indexDiff)
-                set.remove((long) nums[i-indexDiff]);
+                set.remove((long)nums[i-indexDiff]);
         }
         return false;
     }
