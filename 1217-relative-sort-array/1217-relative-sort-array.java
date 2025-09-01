5333226d64456b7682568aa4e134787d9e453e15
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-09-01 21:24:32 +0530

    Time: 2 ms (70.5%), Space: 42.4 MB (30.99%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,31 +1,26 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] freq = new int[1001];
+        int[] ans = new int[arr1.length];
+        Arrays.sort(arr1);
 
-        Map<Integer, Integer> mp = new HashMap<>();
-        for(int i = 0; i < arr2.length; i++){
-            mp.put(arr2[i], i);
+        for(int ele: arr1)
+            freq[ele]++;
+        
+        int i = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int ele: arr2){
+            while(freq[ele]-- > 0){
+                ans[i++] = ele;
+            }
+            set.add(ele);
         }
 
         for(int ele: arr1){
-            if(!mp.containsKey(ele))
-                mp.put(ele, 1000000000);
+            if(!set.contains(ele))
+                ans[i++] = ele;
         }
 
-        List<Integer> list = new ArrayList<>(arr1.length);
-        for (int ele : arr1) 
-            list.add(ele);
-
-        Collections.sort(list, (num1, num2) -> {
-            if(mp.get(num1).equals(mp.get(num2)))
-                return Integer.compare(num1,num2);
-            return Integer.compare(mp.get(num1), mp.get(num2));
-        });
-
-        int i = 0;
-        for(int ele: list){
-            arr1[i++] = ele;
-        }
-
-        return arr1;
+        return ans;
     }
 }
