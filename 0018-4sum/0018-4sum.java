677442671a409edf27a02a11d57e03d66e13a210
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2025-10-26 11:44:10 +0530

    Time: 14 ms (92.98%), Space: 43.8 MB (78.27%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -5,23 +5,30 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         for(int i = 0; i <= n - 4; i++){
+            // handle duplicates for i
             if(i > 0 && nums[i] == nums[i - 1])
                 continue;
+
             for(int j = i + 1; j <= n - 3; j++){
+                // handle duplicates for j
                 if(j > i + 1 && nums[j] == nums[j - 1])
                     continue;
 
+                // use long to avoid integer overflow
                 long newTarget = (long)target - nums[i] - nums[j];
                 int k = j + 1, l = n - 1;
                 while(k < l){
+                    // use long to avoid integer overflow
                     long sum = nums[k] + nums[l];
                     if(sum == newTarget){
                         ans.add(List.of(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
 
+                        // handle duplicates for k
                         while(k < l && nums[k] == nums[k - 1])
                             k++;
+                        // handle duplicates for l
                         while(k < l && nums[l] == nums[l + 1])
                             l--;
                     }
