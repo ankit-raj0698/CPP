ce2487a71bd2517cec1a2d69f239829d94262177
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-01-04 11:52:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.java
+++ Difficulty: Easy/Insertion Sort/insertion-sort.java
@@ -6,10 +6,10 @@ class Solution {
             int key = arr[i];
             int j = i - 1;
             while(j >= 0 && arr[j] > key){
-                arr[j+1] = arr[j];
+                arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
+            arr[j + 1] = key;
         }
     }
 }
