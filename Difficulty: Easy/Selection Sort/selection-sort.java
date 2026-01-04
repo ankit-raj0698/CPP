15e770b34c7a90ac949c8b37028faaf47618ca84
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-01-04 11:35:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.java
+++ Difficulty: Easy/Selection Sort/selection-sort.java
@@ -1,22 +1,16 @@
 class Solution {
-    
-    void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
     void selectionSort(int[] arr) {
         // code here
         for(int i = 0; i < arr.length; i++){
             int minInd = i;
-            for(int j = i; j < arr.length; j++){
+            for(int j = i + 1; j < arr.length; j++){
                 if(arr[j] < arr[minInd])
                     minInd = j;
             }
-            //System.out.println("minInd = " + minInd);
-            // swap curr ind with min ind
-            swap(arr, i, minInd);
+            // swap ele at minInd and i
+            int temp = arr[minInd];
+            arr[minInd] = arr[i];
+            arr[i] = temp;
         }
     }
 }
