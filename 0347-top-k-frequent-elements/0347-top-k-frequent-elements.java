1ab35a297f64708bac532e942b84075e220e5bbe
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-01-07 19:43:27 +0530

    Time: 11 ms (94.84%), Space: 47.6 MB (68.24%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         int n = nums.length;
+        // find the freq of each element
         Map<Integer,Integer> freq = new HashMap<>();
         for(int ele: nums)
             freq.put(ele, freq.getOrDefault(ele,0) + 1);
         
+        // declare and initialize the array where each ind contains list of integer
         List<Integer>[] bucket = new List[n+1];
         for(int i = 0; i < n+1; i++)
             bucket[i] = new ArrayList<>();
 
+        // populate the bucket (freq : ind, element with that freq is in list)
         for(Map.Entry<Integer, Integer> entry: freq.entrySet())
             bucket[entry.getValue()].add(entry.getKey());
 
+        // iterate through the bucket array of list of integers
+        // store most freq k elements in ans array
         int[] ans = new int[k];
         int ind = 0;
         for(int i = n; i >= 0 && ind < k; i--){
