e1d236f88b91d881435ba5afe276f9561fbd3aa0
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-01-07 19:40:53 +0530

    Time: 10 ms (97.04%), Space: 47.5 MB (81.45%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,30 +1,26 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        Map<Integer,Integer> freq = new HashMap<>();
+        for(int ele: nums)
+            freq.put(ele, freq.getOrDefault(ele,0) + 1);
         
-        Map<Integer, Integer> freq = new HashMap<>();
-        List<Integer>[] buckets = new List[nums.length + 1];
+        List<Integer>[] bucket = new List[n+1];
+        for(int i = 0; i < n+1; i++)
+            bucket[i] = new ArrayList<>();
+
+        for(Map.Entry<Integer, Integer> entry: freq.entrySet())
+            bucket[entry.getValue()].add(entry.getKey());
+
         int[] ans = new int[k];
-
-        // 1. Frequency count
-        for (int num : nums)
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-
-        // 2. Fill buckets (lazy initialization)
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
-            int f = entry.getValue();
-            if (buckets[f] == null)
-                buckets[f] = new ArrayList<>();
-            buckets[f].add(entry.getKey());
-        }
-
-        // 3. Collect top k from highest frequency
-        int idx = 0;
-        for (int i = buckets.length - 1; i >= 0 && idx < k; i--) {
-            if (buckets[i] == null) continue;
-            for (int num : buckets[i]) {
-                ans[idx++] = num;
-                if (idx == k)
-                    return ans;
+        int ind = 0;
+        for(int i = n; i >= 0 && ind < k; i--){
+            if(!bucket[i].isEmpty()){
+                for(int j = 0; j < bucket[i].size(); j++){
+                    ans[ind++] = bucket[i].get(j);
+                    if(ind == k)
+                        return ans;
+                }           
             }
         }
         return ans;
