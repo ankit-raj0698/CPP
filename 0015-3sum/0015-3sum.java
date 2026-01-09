0bb073924a7d556538ed1626fce774c277c255d7
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-01-09 21:06:38 +0530

    Time: 36 ms (32.87%), Space: 59.3 MB (48.16%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,36 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int len = nums.length;
-        
-        for(int i = 0; i < len; i++){
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
 
-            // handle duplicates for i
-            if(i > 0 && nums[i] == nums[i-1])
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1])
                 continue;
-
+                
             int target = -nums[i];
             int j = i + 1;
-            int k = len - 1;
+            int k = n - 1;
+
             while(j < k){
                 int sum = nums[j] + nums[k];
                 if(sum == target){
-                    ans.add(List.of(nums[i], nums[j], nums[k]));
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    
-                    // handle duplicates for j
-                    while(j < k && nums[j] == nums[j-1])
+
+                    while(j < k && nums[j] == nums[j - 1])
                         j++;
-                    // handle duplicates for k
-                    while(j < k && nums[k] == nums[k+1])
+                    
+                    while(j < k && nums[k] == nums[k + 1])
                         k--;
                 }
-                else if(sum < target)
+                else if(sum < target){
                     j++;
-                else
+                }   
+                else{
                     k--;
+                }
             }
         }
         return ans;
