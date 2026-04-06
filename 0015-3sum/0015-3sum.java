6ca0e1c48b577663deaca8998cc235c19249ad8e
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-04-06 20:14:03 +0530

    Time: 32 ms (78.43%), Space: 60.4 MB (8.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,13 +1,11 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-
-        for(int i = 0; i < n; i++){
-            if(i > 0 && nums[i] == nums[i - 1])
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < n - 2; i++){
+            if(i > 0 && nums[i] == nums[i -1])
                 continue;
-                
             int target = -nums[i];
             int j = i + 1;
             int k = n - 1;
@@ -15,22 +13,23 @@ class Solution {
             while(j < k){
                 int sum = nums[j] + nums[k];
                 if(sum == target){
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    ans.add(temp);
                     j++;
                     k--;
 
                     while(j < k && nums[j] == nums[j - 1])
                         j++;
-                    
                     while(j < k && nums[k] == nums[k + 1])
                         k--;
                 }
-                else if(sum < target){
+                else if(sum < target)
                     j++;
-                }   
-                else{
+                else
                     k--;
-                }
             }
         }
         return ans;
