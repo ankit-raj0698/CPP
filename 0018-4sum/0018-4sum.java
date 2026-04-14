2eedce488a5fa016040cc9e9c7d9d8bec8ebc755
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-04-14 22:12:14 +0530

    Time: 20 ms (66.59%), Space: 45.8 MB (50.64%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,38 +1,34 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
+
         Arrays.sort(nums);
-        for(int i = 0; i <= n - 4; i++){
-            // handle duplicates for i
+        for(int i = 0; i < n - 3; i++){
             if(i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for(int j = i + 1; j <= n - 3; j++){
-                // handle duplicates for j
+            for(int j = i + 1; j < n - 2; j++){
                 if(j > i + 1 && nums[j] == nums[j - 1])
                     continue;
 
-                // use long to avoid integer overflow
-                long newTarget = (long)target - nums[i] - nums[j];
-                int k = j + 1, l = n - 1;
+                int k = j + 1;
+                int l = n - 1;
+
                 while(k < l){
-                    // use long to avoid integer overflow
-                    long sum = nums[k] + nums[l];
-                    if(sum == newTarget){
-                        ans.add(List.of(nums[i], nums[j], nums[k], nums[l]));
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum == target){
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
                         k++;
                         l--;
 
-                        // handle duplicates for k
                         while(k < l && nums[k] == nums[k - 1])
                             k++;
-                        // handle duplicates for l
                         while(k < l && nums[l] == nums[l + 1])
                             l--;
-                    }
-                    else if(sum < newTarget)
+                    } 
+                    else if(sum < target)
                         k++;
                     else
                         l--;
