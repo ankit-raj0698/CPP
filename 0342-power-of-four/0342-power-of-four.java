6e6e7e718d0ef06bd0dcc438fe99bbd4d22868d3
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-04-26 09:08:22 +0530

    Time: 1 ms (91.47%), Space: 42.4 MB (85.66%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n == 1)
-            return true;
-        if(n == 0 || n % 4 != 0)
+        if(n <= 0 || (n & (n - 1))!= 0)
             return false;
-        
-        return isPowerOfFour(n/4);
+        return (n & 0x55555555) != 0;
     }
 }
