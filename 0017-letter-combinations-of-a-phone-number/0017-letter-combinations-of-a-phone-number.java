bd4f6f5d072c25e9cf44523cf3a08527e8d9c1d3
ANKIT RAJ <157926345+ankit-raj0698@users.noreply.github.com>
2026-05-17 14:12:18 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.88%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        String[] map = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> ans = new ArrayList<>();
+        solve(digits, 0, map, new StringBuilder(), ans);
+        return ans;
+    }
+
+    public void solve(String digits, int start, String[] map, StringBuilder temp, List<String> ans){
+
+        if(start >= digits.length()){
+            ans.add(new String(temp.toString()));
+            return;
+        }
+
+            String str = map[digits.charAt(start) - '0'];
+
+            for(int j = 0; j < str.length(); j++){
+                temp.append(str.charAt(j));
+                solve(digits, start + 1, map, temp, ans);
+                temp.deleteCharAt(temp.length() - 1);
+            
+        }
+    }
+}
